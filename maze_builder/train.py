0995a14c180c5012e31d415c8d3bcf2c1457d4a7
Brent Kerby <blkerby@gmail.com>
2021-06-25 07:35:48 -0600

    splitting off RoomEncoder (going to revert this)

--- maze_builder/train.py
+++ maze_builder/train.py
@@ -53,39 +53,22 @@ class GlobalAvgPool2d(torch.nn.Module):
         return torch.mean(X, dim=[2, 3])
 
 
-class MainNetwork(torch.nn.Module):
-    def __init__(self, conv_channels: List[int], kernel_size: List[int],
-                 fc_hidden_widths: List[int],
-                 room_embedding_width: int, rooms: List[Room],
-                 map_x: int, map_y: int, output_width: int):
+class RoomEncoder(torch.nn.Module):
+    def __init__(self, rooms, room_embedding_width, map_x, map_y):
         super().__init__()
         self.rooms = rooms
         self.map_x = map_x
         self.map_y = map_y
         self.room_embedding_width = room_embedding_width
         self.room_embedding = torch.nn.Parameter(torch.randn([len(rooms), room_embedding_width], dtype=torch.float32))
-        self.conv_widths = [1 + room_embedding_width] + conv_channels
-        self.fc_widths = [conv_channels[-1]] + fc_hidden_widths
-        layers = []
-        for i in range(len(conv_channels)):
-            layers.append(torch.nn.Conv2d(self.conv_widths[i], self.conv_widths[i + 1],
-                                          kernel_size=(kernel_size[i], kernel_size[i]), padding=kernel_size[i] // 2))
-            layers.append(torch.nn.ReLU())
-            layers.append(torch.nn.MaxPool2d(3, stride=2))
-        layers.append(GlobalAvgPool2d())
-        for i in range(len(fc_hidden_widths)):
-            layers.append(torch.nn.Linear(self.fc_widths[i], self.fc_widths[i + 1]))
-            layers.append(torch.nn.ReLU())
-        layers.append(torch.nn.Linear(self.fc_widths[-1], output_width))
-        self.sequential = torch.nn.Sequential(*layers)
 
-    def encode_map(self, room_positions):
+    def forward(self, room_positions):
         n = room_positions.shape[0]
         room_positions = room_positions.view(n, -1, 2)
         room_positions_x = room_positions[:, :, 0]
         room_positions_y = room_positions[:, :, 1]
         map = torch.zeros([n, self.map_x, self.map_y], dtype=torch.float32)
-        embeddings = torch.zeros([n, self.map_x, self.map_y, self.room_embedding_width], dtype=torch.float32)
+        embeddings = torch.zeros([n, self.room_embedding_width, self.map_x, self.map_y], dtype=torch.float32)
         for i, room in enumerate(self.rooms):
             room_map = torch.transpose(torch.tensor(room.map), 0, 1).unsqueeze(0)
             room_index_x = torch.arange(room.width).view(1, -1, 1)
@@ -96,20 +79,54 @@ class MainNetwork(torch.nn.Module):
             index_y = room_index_y + room_y
             map[torch.arange(n).view(-1, 1, 1), index_x, index_y] += room_map
 
-            room_embedding = self.room_embedding[i, :].view(1, 1, 1, -1)
-            filter_room_embedding = room_embedding * room_map.unsqueeze(3)
-            embedding_index = torch.arange(self.room_embedding_width).view(1, 1, 1, -1)
-            embeddings[torch.arange(n).view(-1, 1, 1, 1), index_x.unsqueeze(3), index_y.unsqueeze(3), embedding_index] += filter_room_embedding
+            room_embedding = self.room_embedding[i, :].view(1, -1, 1, 1)
+            filter_room_embedding = room_embedding * room_map.unsqueeze(1)
+            embedding_index = torch.arange(self.room_embedding_width).view(1, -1, 1, 1)
+            embeddings[torch.arange(n).view(-1, 1, 1, 1), embedding_index, index_x.unsqueeze(1), index_y.unsqueeze(1)] += filter_room_embedding
 
-        out = torch.cat([map.unsqueeze(3), embeddings], dim=3)
-        out = torch.transpose(out, 1, 3)  # TODO: Clean this up (channel dim was in Tensorflow-style last position)
+        out = torch.cat([map.unsqueeze(1), embeddings], dim=1)
+        # out = torch.transpose(out, 1, 3)  # TODO: Clean this up (channel dim was in Tensorflow-style last position)
         return out
 
+
+class MainNetwork(torch.nn.Module):
+    def __init__(self, conv_channels: List[int], kernel_size: List[int],
+                 fc_hidden_widths: List[int],
+                 room_embedding_width: int, rooms: List[Room],
+                 map_x: int, map_y: int, output_width: int):
+        super().__init__()
+        self.rooms = rooms
+        self.map_x = map_x
+        self.map_y = map_y
+        self.room_embedding_width = room_embedding_width
+        self.conv_widths = [1 + room_embedding_width] + conv_channels
+        self.fc_widths = [conv_channels[-1]] + fc_hidden_widths
+        layers = []
+        layers.append(RoomEncoder(rooms, room_embedding_width, map_x, map_y))
+        for i in range(len(conv_channels)):
+            layers.append(torch.nn.Conv2d(self.conv_widths[i], self.conv_widths[i + 1],
+                                          kernel_size=(kernel_size[i], kernel_size[i]), padding=kernel_size[i] // 2))
+            layers.append(torch.nn.ReLU())
+            # layers.append(torch.nn.BatchNorm2d(self.conv_widths[i + 1]))
+            layers.append(torch.nn.MaxPool2d(3, stride=2))
+        layers.append(GlobalAvgPool2d())
+        for i in range(len(fc_hidden_widths)):
+            layers.append(torch.nn.Linear(self.fc_widths[i], self.fc_widths[i + 1]))
+            layers.append(torch.nn.ReLU())
+            # layers.append(torch.nn.BatchNorm1d(self.fc_widths[i + 1]))
+        layers.append(torch.nn.Linear(self.fc_widths[-1], output_width))
+        self.sequential = torch.nn.Sequential(*layers)
+
+
+    # def decode_map(self, X, room_positions):
+    #     for i, room in enumerate(self.rooms):
+    #
+
     def forward(self, X):
-        X = self.encode_map(X)
+        # X = self.encode_map(room_positions)
         for layer in self.sequential:
             X = layer(X)
-            # print(layer, X)
+            # print(X.shape, layer)
         # out = self.sequential(E)
         return X
 
@@ -134,13 +151,13 @@ class Model(torch.nn.Module):
         # self.policy_layer = torch.nn.Linear(widths[-1], num_actions)
         # self.value_layer = torch.nn.Linear(widths[-1], 1)
         self.main_network = torch.nn.Sequential()
-        self.policy_layer = MainNetwork(conv_channels=[16, 32, 64],
-                            kernel_size=[3, 3, 2], fc_hidden_widths=[128],
+        self.policy_layer = MainNetwork(conv_channels=[8],
+                            kernel_size=[3], fc_hidden_widths=[64],
                             room_embedding_width=3,
                             rooms=rooms, map_x=12, map_y=12,
                             output_width=num_actions)
-        self.value_layer = MainNetwork(conv_channels=[16, 32, 64],
-                            kernel_size=[3, 3, 2], fc_hidden_widths=[64],
+        self.value_layer = MainNetwork(conv_channels=[8],
+                            kernel_size=[3], fc_hidden_widths=[64],
                             room_embedding_width=3,
                             rooms=rooms, map_x=12, map_y=12,
                             output_width=1)
@@ -232,14 +249,17 @@ class ReplayBuffer():
 
 class TrainingSession():
     def __init__(self, env: gym.Env, model: Model,
-                 optimizer: torch.optim.Optimizer,
+                 value_optimizer: torch.optim.Optimizer,
+                 policy_optimizer: torch.optim.Optimizer,
                  replay_capacity: int, reward_horizon: int, max_steps: int,
                  value_loss_coef: float,
                  entropy_penalty: float,
                  weight_decay: float):
         self.env = env
         self.model = model
-        self.optimizer = optimizer
+        # self.optimizer = optimizer
+        self.value_optimizer = value_optimizer
+        self.policy_optimizer = policy_optimizer
         self.reward_horizon = reward_horizon
         self.max_steps = max_steps
         self.value_loss_coef = value_loss_coef
@@ -254,6 +274,7 @@ class TrainingSession():
         self.observation = env.reset().reshape(-1)
 
     def play_step(self):
+        self.model.eval()
         X = torch.from_numpy(self.observation).unsqueeze(0).view(1, -1)
         with torch.no_grad():
             p_raw = model.forward_policy(X)
@@ -264,7 +285,6 @@ class TrainingSession():
         self.step_number += 1
         self.total_steps += 1
         artificial_end = self.step_number == self.max_steps
-        # print("after: self.o=", self.observation, "o=", observation)
         self.replay.append(self.observation, observation, action, reward, done, artificial_end)
         if done or artificial_end:
             self.observation = self.env.reset().reshape(-1)
@@ -274,6 +294,7 @@ class TrainingSession():
             self.observation = observation
 
     def train_step(self, batch_size):
+        self.model.train()
         state1, state2, action, mean_reward = self.replay.sample(batch_size)
         p_raw, value1 = self.model.forward_full(state1)
         with torch.no_grad():
@@ -287,19 +308,19 @@ class TrainingSession():
         # entropy = -torch.sum(p_log * torch.exp(p_log))
         entropy = torch.sum(torch.mean(p_raw ** 2, dim=1))
         entropy_loss = self.entropy_penalty * entropy
-        loss = (policy_loss + value_loss + entropy_loss) / batch_size
-        self.optimizer.zero_grad()
-        loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1e-5)
-        self.optimizer.step()
+        # loss = (policy_loss + value_loss + entropy_loss) / batch_size
+        # self.optimizer.zero_grad()
+        # loss.backward()
+        # torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1e-5)
+        # self.optimizer.step()
 
-        # self.policy_optimizer.zero_grad()
-        # policy_loss.backward()
-        # self.policy_optimizer.step()
-        #
-        # self.value_optimizer.zero_grad()
-        # value_loss.backward()
-        # self.value_optimizer.step()
+        self.value_optimizer.zero_grad()
+        value_loss.backward()
+        self.value_optimizer.step()
+
+        self.policy_optimizer.zero_grad()
+        (policy_loss + entropy_loss).backward()
+        self.policy_optimizer.step()
 
         self.model.weight_decay(self.weight_decay)
         return policy_loss.item(), value_loss.item(), entropy.item()
@@ -311,26 +332,30 @@ env = MazeBuilderEnv(maze_builder.crateria.rooms[:10], map_x=12, map_y=12, actio
 observation_dim = np.prod(env.observation_space.shape)
 action_dim = env.action_space.n
 model = Model(action_dim, env.rooms)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, betas=(0.9, 0.9), eps=1e-15)
+model.policy_layer.sequential[-1].weight.data[:, :] *= 0.01
+model.policy_layer.sequential[-1].bias.data[:] = 0.0
+model.value_layer.sequential[-1].weight.data[:, :] *= 0.01
+model.value_layer.sequential[-1].bias.data[:] = 0.0
+value_optimizer = torch.optim.Adam(model.value_layer.parameters(), lr=0.002, betas=(0.9, 0.999), eps=1e-15)
+policy_optimizer = torch.optim.Adam(model.policy_layer.parameters(), lr=0.00005, betas=(0.9, 0.999), eps=1e-15)
 print(model)
 # optimizer = torch.optim.SGD(model.parameters(), lr=0.05)
-# batch_size = 2048
-# train_freq = 64
-batch_size = 256
+batch_size = 2048
 train_freq = 64
 print_freq = 64
 display_freq = 64
 session = TrainingSession(env, model,
-                          optimizer,
+                          value_optimizer,
+                          policy_optimizer,
                           replay_capacity=5000, reward_horizon=10,
                           max_steps=200, value_loss_coef=1.0,
-                          weight_decay=0.0 * optimizer.param_groups[0]['lr'],
-                          entropy_penalty=0.05)
+                          weight_decay=0.0, # * value_optimizer.param_groups[0]['lr'],
+                          entropy_penalty=0.02)
 
-entropy_penalty0 = 0.05
-entropy_penalty1 = 0.05
-lr0 = 0.0002
-lr1 = 0.0002
+# entropy_penalty0 = 0.05
+# entropy_penalty1 = 0.05
+# lr0 = 0.0003
+# lr1 = 0.0003
 transition_time = 200000
 total_policy_loss = 0.0
 total_value_loss = 0.0
@@ -342,10 +367,10 @@ while True:
     if session.episode_number % display_freq == 0:
         env.render()
     if session.replay.size >= batch_size and session.total_steps % train_freq == 0:
-        lr = np.interp(session.total_steps, [0, transition_time], [lr0, lr1])
-        entropy_penalty = np.interp(session.total_steps, [0, transition_time], [entropy_penalty0, entropy_penalty1])
-        session.optimizer.param_groups[0]['lr'] = lr
-        session.entropy_penalty = entropy_penalty
+        # lr = np.interp(session.total_steps, [0, transition_time], [lr0, lr1])
+        # entropy_penalty = np.interp(session.total_steps, [0, transition_time], [entropy_penalty0, entropy_penalty1])
+        # session.optimizer.param_groups[0]['lr'] = lr
+        # session.entropy_penalty = entropy_penalty
 
         policy_loss, value_loss, entropy = session.train_step(batch_size)
         total_policy_loss += policy_loss
