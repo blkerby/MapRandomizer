6ea577828d18bc85d6bc8d88765cf17014e4db7e
Brent Kerby <blkerby@gmail.com>
2021-07-17 17:34:37 -0600

    trying batch norm again (poor results, maybe not playing nice with parameter averaging

--- model_average.py
+++ model_average.py
@@ -6,28 +6,23 @@ import torch
 
 class SimpleAverage:
     """
-    Accumulates a simple average of a set of parameters.
-
-    Args:
-        parameters: Iterable of `torch.nn.Parameter` (typically from
-            `model.parameters()`).
+    Accumulates a simple average of a set of tensors.
     """
-    def __init__(self, params: Iterable[torch.nn.Parameter]):
-        self.params = list(params)
-        self.shadow_params_sum = [p.data.clone() for p in self.params]
+    def __init__(self, params):
+        self.shadow_params_sum = [p.clone() for p in params]
         self.shadow_params = None
         self.stored_params = None
         self.count = 1
 
-    def update(self):
+    def update(self, params):
         """
         Update currently maintained parameters.
 
         Call this every time the parameters are updated, such as the result of
         the `optimizer.step()` call.
         """
-        for i, param in enumerate(self.params):
-            self.shadow_params_sum[i] += param.data
+        for i, param in enumerate(params):
+            self.shadow_params_sum[i] += param
         self.count += 1
         self.shadow_params = None
 
@@ -35,23 +30,23 @@ class SimpleAverage:
         if self.shadow_params is None:
             self.shadow_params = [p / self.count for p in self.shadow_params_sum]
 
-    def use_averages(self):
+    def use_averages(self, params):
         self.compute_shadow_params()
-        for i, param in enumerate(self.params):
-            param.data = self.shadow_params[i]
+        for i, param in enumerate(params):
+            param.copy_(self.shadow_params[i])
 
     def reset(self):
         for param in self.shadow_params_sum:
             param.zero_()
         self.count = 0
 
-    def store(self) -> None:
+    def store(self, params) -> None:
         """
         Save the current parameters for restoring later.
         """
-        self.stored_params = [param.data.clone() for param in self.params]
+        self.stored_params = [param.clone() for param in params]
 
-    def restore(self):
+    def restore(self, params):
         """
         Restore the parameters stored with the `store` method.
         Useful to validate the model with averaged parameters without affecting the
@@ -59,18 +54,18 @@ class SimpleAverage:
         `use_averages` method. After validation (or model saving), use this to
         restore the former parameters.
         """
-        for i, param in enumerate(self.params):
-            param.data = self.stored_params[i]
+        for i, param in enumerate(params):
+            param.copy_(self.stored_params[i])
         self.stored_params = None
 
     @contextlib.contextmanager
-    def average_parameters(self):
+    def average_parameters(self, params):
         r"""
         Context manager for validation/inference with averaged parameters.
         """
-        self.store()
-        self.use_averages()
+        self.store(params)
+        self.use_averages(params)
         try:
             yield
         finally:
-            self.restore()
+            self.restore(params)
