5d11082b986c48ce003bcb96d8bb00f0c18a11ee
Brent Kerby <blkerby@gmail.com>
2022-01-16 12:37:05 -0700

    continue training, set up for running on cpu

--- maze_builder/train.py
+++ maze_builder/train.py
@@ -173,159 +173,159 @@ torch.set_printoptions(linewidth=120, threshold=10000)
 # #     start_i = session.replay_buffer.size * i // num_groups
 # #     end_i = session.replay_buffer.size * (i + 1) // num_groups
 # #     print(start_i, max_possible_reward - torch.mean(session.replay_buffer.episode_data.reward[start_i:end_i].to(torch.float32)))
-
-gen_print_freq = 32
-i = 0
-total_reward = 0
-total_reward2 = 0
-cnt_episodes = 0
-while session.replay_buffer.size < session.replay_buffer.capacity:
-    data = session.generate_round(
-        episode_length=episode_length,
-        num_candidates=1,
-        temperature=1e-10,
-        # num_candidates=32,
-        # temperature=1e-4,
-        explore_eps=0.0,
-        render=False,
-        executor=executor)
-    session.replay_buffer.insert(data)
-
-    total_reward += torch.sum(data.reward.to(torch.float32)).item()
-    total_reward2 += torch.sum(data.reward.to(torch.float32) ** 2).item()
-    cnt_episodes += data.reward.shape[0]
-
-    i += 1
-    if i % gen_print_freq == 0:
-        mean_reward = total_reward / cnt_episodes
-        std_reward = math.sqrt(total_reward2 / cnt_episodes - mean_reward ** 2)
-        ci_reward = std_reward * 1.96 / math.sqrt(cnt_episodes)
-        logging.info("init gen {}/{}: cost={:.3f} +/- {:.3f}".format(
-            i, session.replay_buffer.capacity // (num_envs * num_devices),
-               max_possible_reward - mean_reward, ci_reward))
 #
-# # for i, param in enumerate(session.model.parameters()):
-# #     if param.shape != session.average_parameters.shadow_params[i].shape:
-# #         print(i, param.shape, session.average_parameters.shadow_params[i].shape)
+# gen_print_freq = 32
+# i = 0
+# total_reward = 0
+# total_reward2 = 0
+# cnt_episodes = 0
+# while session.replay_buffer.size < session.replay_buffer.capacity:
+#     data = session.generate_round(
+#         episode_length=episode_length,
+#         num_candidates=1,
+#         temperature=1e-10,
+#         # num_candidates=32,
+#         # temperature=1e-4,
+#         explore_eps=0.0,
+#         render=False,
+#         executor=executor)
+#     session.replay_buffer.insert(data)
 #
-# # for i in range(20):
-# #     start = i * 1000 + 150000
-# #     end = start + 1000
-# #     reward = session.replay_buffer.episode_data.reward[start:end]
-# #     print(start, end, torch.mean(reward.to(torch.float32)))
+#     total_reward += torch.sum(data.reward.to(torch.float32)).item()
+#     total_reward2 += torch.sum(data.reward.to(torch.float32) ** 2).item()
+#     cnt_episodes += data.reward.shape[0]
 #
+#     i += 1
+#     if i % gen_print_freq == 0:
+#         mean_reward = total_reward / cnt_episodes
+#         std_reward = math.sqrt(total_reward2 / cnt_episodes - mean_reward ** 2)
+#         ci_reward = std_reward * 1.96 / math.sqrt(cnt_episodes)
+#         logging.info("init gen {}/{}: cost={:.3f} +/- {:.3f}".format(
+#             i, session.replay_buffer.capacity // (num_envs * num_devices),
+#                max_possible_reward - mean_reward, ci_reward))
 # #
-# # num_eval_rounds = session.replay_buffer.size // (num_envs * num_devices) // 32
-# # eval_data_list = []
-# # for j in range(num_eval_rounds):
-# #     eval_data = session.generate_round(
-# #         episode_length=episode_length,
-# #         num_candidates=num_candidates,
-# #         temperature=temperature1,
-# #         explore_eps=explore_eps,
-# #         render=False,
-# #         executor=executor)
-# #     if j % print_freq == 0:
-# #         logging.info("init eval {}/{}".format(j, num_eval_rounds))
-# #     eval_data_list.append(eval_data)
-# # eval_data = EpisodeData(
-# #     reward=torch.cat([x.reward for x in eval_data_list], dim=0),
-# #     door_connects=torch.cat([x.door_connects for x in eval_data_list], dim=0),
-# #     action=torch.cat([x.action for x in eval_data_list], dim=0),
-# #     prob=torch.cat([x.prob for x in eval_data_list], dim=0),
-# #     test_loss=torch.cat([x.test_loss for x in eval_data_list], dim=0),
-# # )
+# # # for i, param in enumerate(session.model.parameters()):
+# # #     if param.shape != session.average_parameters.shadow_params[i].shape:
+# # #         print(i, param.shape, session.average_parameters.shadow_params[i].shape)
+# #
+# # # for i in range(20):
+# # #     start = i * 1000 + 150000
+# # #     end = start + 1000
+# # #     reward = session.replay_buffer.episode_data.reward[start:end]
+# # #     print(start, end, torch.mean(reward.to(torch.float32)))
+# #
+# # #
+# # # num_eval_rounds = session.replay_buffer.size // (num_envs * num_devices) // 32
+# # # eval_data_list = []
+# # # for j in range(num_eval_rounds):
+# # #     eval_data = session.generate_round(
+# # #         episode_length=episode_length,
+# # #         num_candidates=num_candidates,
+# # #         temperature=temperature1,
+# # #         explore_eps=explore_eps,
+# # #         render=False,
+# # #         executor=executor)
+# # #     if j % print_freq == 0:
+# # #         logging.info("init eval {}/{}".format(j, num_eval_rounds))
+# # #     eval_data_list.append(eval_data)
+# # # eval_data = EpisodeData(
+# # #     reward=torch.cat([x.reward for x in eval_data_list], dim=0),
+# # #     door_connects=torch.cat([x.door_connects for x in eval_data_list], dim=0),
+# # #     action=torch.cat([x.action for x in eval_data_list], dim=0),
+# # #     prob=torch.cat([x.prob for x in eval_data_list], dim=0),
+# # #     test_loss=torch.cat([x.test_loss for x in eval_data_list], dim=0),
+# # # )
+# #
+# session.replay_buffer.episode_data.prob[:] = 1 / num_candidates
+# pickle.dump(session, open('init_session.pkl', 'wb'))
+# # session = pickle.load(open('init_session.pkl', 'rb'))
 #
-session.replay_buffer.episode_data.prob[:] = 1 / num_candidates
-pickle.dump(session, open('init_session.pkl', 'wb'))
-# session = pickle.load(open('init_session.pkl', 'rb'))
-
-
-# # pickle.dump(eval_data, open('eval_data2.pkl', 'wb'))
 #
-# # session2 = pickle.load(open('init_session.pkl', 'rb'))
-# # session.replay_buffer = session2.replay_buffer
-# # eval_data = pickle.load(open('eval_data2.pkl', 'rb'))
-#
-#
-# # teacher_model = session.model
-# # session.network = make_network()
-num_eval_rounds = session.replay_buffer.size // (num_envs * num_devices) // 64
-# session.model = Model(
+# # # pickle.dump(eval_data, open('eval_data2.pkl', 'wb'))
+# #
+# # # session2 = pickle.load(open('init_session.pkl', 'rb'))
+# # # session.replay_buffer = session2.replay_buffer
+# # # eval_data = pickle.load(open('eval_data2.pkl', 'rb'))
+# #
+# #
+# # # teacher_model = session.model
+# # # session.network = make_network()
+# num_eval_rounds = session.replay_buffer.size // (num_envs * num_devices) // 64
+# # session.model = Model(
+# #     env_config=env_config,
+# #     num_doors=envs[0].num_doors,
+# #     num_missing_connects=envs[0].num_missing_connects,
+# #     num_room_parts=len(envs[0].good_room_parts),
+# #     map_channels=[32, 64, 128],
+# #     map_stride=[2, 2, 2],
+# #     map_kernel_size=[7, 3, 3],
+# #     map_padding=3 * [False],
+# #     room_embedding_width=6,
+# #     connectivity_in_width=64,
+# #     connectivity_out_width=256,
+# #     fc_widths=[1024, 256, 64],
+# #     global_dropout_p=0.0,
+# # ).to(device)
+# # session = pickle.load(open('models/session-2021-10-08T16:18:17.471054.pkl', 'rb'))
+# session.model = DoorLocalModel(
 #     env_config=env_config,
 #     num_doors=envs[0].num_doors,
 #     num_missing_connects=envs[0].num_missing_connects,
 #     num_room_parts=len(envs[0].good_room_parts),
-#     map_channels=[32, 64, 128],
-#     map_stride=[2, 2, 2],
-#     map_kernel_size=[7, 3, 3],
-#     map_padding=3 * [False],
-#     room_embedding_width=6,
+#     map_channels=4,
+#     map_kernel_size=16,
 #     connectivity_in_width=64,
-#     connectivity_out_width=256,
-#     fc_widths=[1024, 256, 64],
-#     global_dropout_p=0.0,
+#     local_widths=[256, 0],
+#     global_widths=[256, 256],
+#     # local_widths=[256, 256],
+#     # global_widths=[256, 256],
+#     fc_widths=[512, 512, 512],
+#     alpha=2.0,
 # ).to(device)
-# session = pickle.load(open('models/session-2021-10-08T16:18:17.471054.pkl', 'rb'))
-session.model = DoorLocalModel(
-    env_config=env_config,
-    num_doors=envs[0].num_doors,
-    num_missing_connects=envs[0].num_missing_connects,
-    num_room_parts=len(envs[0].good_room_parts),
-    map_channels=4,
-    map_kernel_size=16,
-    connectivity_in_width=64,
-    local_widths=[256, 0],
-    global_widths=[256, 256],
-    # local_widths=[256, 256],
-    # global_widths=[256, 256],
-    fc_widths=[512, 512, 512],
-    alpha=2.0,
-).to(device)
-session.model.state_value_lin.weight.data.zero_()
-session.model.state_value_lin.bias.data.zero_()
-logging.info(session.model)
-session.average_parameters = ExponentialAverage(session.model.all_param_data(), beta=session.average_parameters.beta)
-# session.optimizer = torch.optim.RMSprop(session.network.parameters(), lr=0.001, alpha=0.95)
-# session.optimizer = torch.optim.RMSprop(session.model.parameters(), lr=0.0002, alpha=0.99)
-session.optimizer = torch.optim.Adam(session.model.parameters(), lr=0.0001, betas=(0.95, 0.99), eps=1e-15)
-# session.optimizer = shampoo.Shampoo(session.model.parameters(), beta=0.999, lr=0.001, update_freq=50)
-# session.optimizer = torch.optim.SGD(session.network.parameters(), lr=0.0005)
-logging.info(session.optimizer)
-# session.optimizer = torch.optim.RMSprop(session.network.parameters(), lr=0.002, alpha=0.95)
-# batch_size = 2 ** batch_size_pow0
-batch_size = 2048
-eval_batch_size = 16
-num_steps = session.replay_buffer.capacity // num_envs
-num_train_batches = int(pass_factor * session.replay_buffer.capacity * episode_length // batch_size // num_steps)
-num_eval_batches = num_eval_rounds * num_envs // eval_batch_size
-print_freq = 64
-eval_freq = print_freq
-# for layer in session.network.global_dropout_layers:
-#     layer.p = 0.0
-init_train_round = 1
-
-# session.optimizer.param_groups[0]['lr'] = 0.99
-# session.optimizer.param_groups[0]['betas'] = (0.95, 0.99)
-# session.optimizer.param_groups[0]['betas'] = (0.998, 0.998)
-session.average_parameters.beta = 0.99
-session.sam_scale = None
-session.decay_amount = 0.0
-# session.model.global_dropout_p = 0.1
-
-# num_steps = 128
-gen_freq = 4
-ema_beta = 0.9
-ema_reward = 0.0
-ema_perfect = 0.0
-ema_weight = 0.0
-student_frac = 0.0
-
-num_total_batches = num_train_batches * num_steps
-lr0_init = 0.0005
-lr1_init = 0.00002
-student_frac_increment = 0.01
-threshold = 0.3
+# session.model.state_value_lin.weight.data.zero_()
+# session.model.state_value_lin.bias.data.zero_()
+# logging.info(session.model)
+# session.average_parameters = ExponentialAverage(session.model.all_param_data(), beta=session.average_parameters.beta)
+# # session.optimizer = torch.optim.RMSprop(session.network.parameters(), lr=0.001, alpha=0.95)
+# # session.optimizer = torch.optim.RMSprop(session.model.parameters(), lr=0.0002, alpha=0.99)
+# session.optimizer = torch.optim.Adam(session.model.parameters(), lr=0.0001, betas=(0.95, 0.99), eps=1e-15)
+# # session.optimizer = shampoo.Shampoo(session.model.parameters(), beta=0.999, lr=0.001, update_freq=50)
+# # session.optimizer = torch.optim.SGD(session.network.parameters(), lr=0.0005)
+# logging.info(session.optimizer)
+# # session.optimizer = torch.optim.RMSprop(session.network.parameters(), lr=0.002, alpha=0.95)
+# # batch_size = 2 ** batch_size_pow0
+# batch_size = 2048
+# eval_batch_size = 16
+# num_steps = session.replay_buffer.capacity // num_envs
+# num_train_batches = int(pass_factor * session.replay_buffer.capacity * episode_length // batch_size // num_steps)
+# num_eval_batches = num_eval_rounds * num_envs // eval_batch_size
+# print_freq = 64
+# eval_freq = print_freq
+# # for layer in session.network.global_dropout_layers:
+# #     layer.p = 0.0
+# init_train_round = 1
+#
+# # session.optimizer.param_groups[0]['lr'] = 0.99
+# # session.optimizer.param_groups[0]['betas'] = (0.95, 0.99)
+# # session.optimizer.param_groups[0]['betas'] = (0.998, 0.998)
+# session.average_parameters.beta = 0.99
+# session.sam_scale = None
+# session.decay_amount = 0.0
+# # session.model.global_dropout_p = 0.1
+#
+# # num_steps = 128
+# gen_freq = 4
+# ema_beta = 0.9
+# ema_reward = 0.0
+# ema_perfect = 0.0
+# ema_weight = 0.0
+# student_frac = 0.0
+#
+# num_total_batches = num_train_batches * num_steps
+# lr0_init = 0.0005
+# lr1_init = 0.00002
+# student_frac_increment = 0.01
+# threshold = 0.3
 # student_frac_inc = 0.01
 # logging.info("Initial training")
 # while init_train_round <= num_steps:
@@ -439,7 +439,16 @@ threshold = 0.3
 # session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk20', 'rb'))
 # session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk21', 'rb'))
 # session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk22', 'rb'))
-session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk23', 'rb'))
+# session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk23', 'rb'))
+# session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk34', 'rb'))
+# session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk38', 'rb'))
+# session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk41', 'rb'))
+# session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk44', 'rb'))
+# session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl', 'rb'))
+# session = pickle.load(open('models/session-2021-12-30T14:27:35.034335.pkl-bk46', 'rb'))
+# session = pickle.load(open('models/session-2021-12-30T21:07:07.735373.pkl', 'rb'))
+# session = pickle.load(open('models/session-2021-12-31T17:27:10.465887.pkl', 'rb'))
+session = pickle.load(open('models/session-2022-01-01T21:25:32.844343.pkl-bk52', 'rb'))
 #
 #
 # session = pickle.load(open('models/session-2021-12-10T06:00:58.163492.pkl-bk19', 'rb'))
@@ -527,23 +536,21 @@ session = pickle.load(open('models/session-2021-12-20T20:50:57.114928.pkl-bk23',
 
 batch_size_pow0 = 11
 batch_size_pow1 = 11
-lr0 = 2e-7
-lr1 = 2e-7
-# lr0 = 1e-30  # TODO: increase this back to 1e-5?
-# lr1 = 1e-30
-num_candidates0 = 61
-num_candidates1 = 61
+lr0 = 5e-6
+lr1 = 5e-6
+num_candidates0 = 45
+num_candidates1 = 64
 num_candidates = num_candidates0
-temperature0 = 0.0025
-temperature1 = 0.0025
-explore_eps0 = 0.002
-explore_eps1 = 0.002
-annealing_start = 392493
-annealing_time = 8000
-pass_factor = 0.5
-alpha0 = 0.1
-alpha1 = 0.1
-print_freq = 16
+temperature0 = 0.002
+temperature1 = 0.002
+explore_eps0 = 0.0002
+explore_eps1 = 0.0002
+annealing_start = 611264
+annealing_time = 16000
+pass_factor = 2.0
+alpha0 = 0.2
+alpha1 = 0.2
+print_freq = 32
 total_reward = 0
 total_loss = 0.0
 total_loss_cnt = 0
@@ -551,16 +558,16 @@ total_test_loss = 0.0
 total_prob = 0.0
 total_round_cnt = 0
 save_freq = 256
-summary_freq = 512
-session.decay_amount = 5.0
+summary_freq = 1024
+session.decay_amount = 0.05
 
 min_door_value = max_possible_reward
 total_min_door_frac = 0
 logging.info("Checkpoint path: {}".format(pickle_name))
 num_params = sum(torch.prod(torch.tensor(list(param.shape))) for param in session.model.parameters())
 logging.info(
-    "map_x={}, map_y={}, num_envs={}, lr0={}, lr1={}, num_candidates0={}, num_candidates1={}, replay_size={}/{}, num_params={}, decay_amount={}, temp0={}, temp1={}, eps0={}, eps1={}, alpha0={}, alpha1={}".format(
-        map_x, map_y, session.envs[0].num_envs, lr0, lr1, num_candidates0, num_candidates1, session.replay_buffer.size,
+    "map_x={}, map_y={}, num_envs={}, pass_factor={}, lr0={}, lr1={}, num_candidates0={}, num_candidates1={}, replay_size={}/{}, num_params={}, decay_amount={}, temp0={}, temp1={}, eps0={}, eps1={}, alpha0={}, alpha1={}".format(
+        map_x, map_y, session.envs[0].num_envs, pass_factor, lr0, lr1, num_candidates0, num_candidates1, session.replay_buffer.size,
         session.replay_buffer.capacity, num_params, session.decay_amount,
         temperature0, temperature1, explore_eps0, explore_eps1, alpha0, alpha1))
 logging.info("Starting training")
@@ -664,7 +671,7 @@ for i in range(1000000):
             # episode_data = session.replay_buffer.episode_data
             # session.replay_buffer.episode_data = None
             pickle.dump(session, open(pickle_name, 'wb'))
-            # pickle.dump(session, open(pickle_name + '-bk27', 'wb'))
+            # pickle.dump(session, open(pickle_name + '-bk52', 'wb'))
             # session.replay_buffer.episode_data = episode_data
             # session = pickle.load(open(pickle_name + '-bk6', 'rb'))
     if session.num_rounds % summary_freq == 0:
--- maze_builder/view.py
+++ maze_builder/view.py
@@ -7,6 +7,7 @@ from maze_builder.types import reconstruct_room_data, Direction
 import logic.rooms.all_rooms
 import pickle
 import concurrent.futures
+import random
 
 logging.basicConfig(format='%(asctime)s %(message)s',
                     level=logging.INFO,
@@ -56,21 +57,35 @@ device = torch.device('cpu')
 # session = CPU_Unpickler(open('models/12-15-session-2021-12-10T06:00:58.163492.pkl', 'rb')).load()
 # session = CPU_Unpickler(open('models/12-18-session-2021-12-10T06:00:58.163492.pkl', 'rb')).load()
 # session = CPU_Unpickler(open('models/12-18-b-session-2021-12-10T06:00:58.163492.pkl', 'rb')).load()
-session = CPU_Unpickler(open('models/12-19-session-2021-12-10T06:00:58.163492.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/12-19-session-2021-12-10T06:00:58.163492.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/12-22-session-2021-12-20T20:50:57.114928.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/12-27-session-2021-12-20T20:50:57.114928.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/12-31-session-2021-12-30T21:07:07.735373.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/01-02-session-2022-01-01T21:25:32.844343.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/01-05-session-2022-01-01T21:25:32.844343.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/01-08-session-2022-01-01T21:25:32.844343.pkl', 'rb')).load()
+# session = CPU_Unpickler(open('models/01-11-session-2022-01-01T21:25:32.844343.pkl', 'rb')).load()
+session = CPU_Unpickler(open('models/01-16-session-2022-01-13T12:40:37.881929.pkl', 'rb')).load()
 #
 
 print(torch.sort(torch.sum(session.replay_buffer.episode_data.missing_connects.to(torch.float32), dim=0)))
 print(torch.max(session.replay_buffer.episode_data.reward))
 
-ind = torch.nonzero(session.replay_buffer.episode_data.reward >= 341)
-ind = ind[(ind >= 200000) & (ind < 262144)].view(-1, 1)
-i = 2
+ind = torch.nonzero(session.replay_buffer.episode_data.reward >= 342)
+# ind = ind[(ind >= 200000) & (ind < 262144)].view(-1, 1)
+i = int(random.randint(0, ind.shape[0] - 1))
 num_rooms = len(session.envs[0].rooms)
 action = session.replay_buffer.episode_data.action[ind[i], :]
 step_indices = torch.tensor([num_rooms])
 room_mask, room_position_x, room_position_y = reconstruct_room_data(action, step_indices, num_rooms)
+
+print(torch.where(session.replay_buffer.episode_data.missing_connects[ind[i, 0], :] == False))
+print(torch.where(room_mask[0, :251] == False))
+print(torch.where(session.replay_buffer.episode_data.door_connects[ind[i, 0], :] == False))
+dir(session.envs[0])
+
 #
-num_envs = 1
+num_envs = 2
 # num_envs = 8
 rooms = logic.rooms.all_rooms.rooms
 
@@ -114,89 +129,94 @@ env.room_position_x = room_position_x
 env.room_position_y = room_position_y
 env.render(0)
 
-for i in range(env.missing_connection_src.shape[0]):
-    print(i, env.rooms[env.part_room_id[env.missing_connection_src[i]]].name)
-
-for i in range(env.part_room_id.shape[0]):
-    print(i, env.rooms[env.part_room_id[i]].name)
-
-
-# import time
-# start = time.perf_counter()
-# adjacency_matrix, A = env.compute_component_matrix(room_mask, room_position_x, room_position_y)
-# end = time.perf_counter()
-# print(end - start)
+# # for i in range(env.missing_connection_src.shape[0]):
+# #     print(i, env.rooms[env.part_room_id[env.missing_connection_src[i]]].name)
+# #
+# # for i in range(env.part_room_id.shape[0]):
+# #     print(i, env.rooms[env.part_room_id[i]].name)
 #
-# edges = torch.nonzero(adjacency_matrix)
-# import graph_tool
-# import graph_tool.topology
 #
-# flat_edges0 = edges[:, 1:3]
-# flat_edges1 = flat_edges0 + 301
-# # flat_edges = torch.cat([flat_edges0, flat_edges1], dim=0)
-# flat_edges = flat_edges0
-# start = time.perf_counter()
-# g = graph_tool.Graph()
-# g.add_edge_list(flat_edges.numpy())
-# # gc = graph_tool.topology.transitive_closure(g)
-# gc = graph_tool.topology.label_components(g)
-# end = time.perf_counter()
-# print(end - start)
-# out_edges = gc.get_edges()
-# env.render()
+# # import time
+# # start = time.perf_counter()
+# # adjacency_matrix, A = env.compute_component_matrix(room_mask, room_position_x, room_position_y)
+# # end = time.perf_counter()
+# # print(end - start)
+# #
+# # edges = torch.nonzero(adjacency_matrix)
+# # import graph_tool
+# # import graph_tool.topology
+# #
+# # flat_edges0 = edges[:, 1:3]
+# # flat_edges1 = flat_edges0 + 301
+# # # flat_edges = torch.cat([flat_edges0, flat_edges1], dim=0)
+# # flat_edges = flat_edges0
+# # start = time.perf_counter()
+# # g = graph_tool.Graph()
+# # g.add_edge_list(flat_edges.numpy())
+# # # gc = graph_tool.topology.transitive_closure(g)
+# # gc = graph_tool.topology.label_components(g)
+# # end = time.perf_counter()
+# # print(end - start)
+# # out_edges = gc.get_edges()
+# # env.render()
+# #
 #
-
-episode_length = len(rooms)
-session.env = None
-session.envs = [env]
-num_candidates = 44
-temperature = 0.01
-torch.manual_seed(0)
-max_possible_reward = env.max_reward
-start_time = time.perf_counter()
-# executor = concurrent.futures.ThreadPoolExecutor(1)
-# for i in range(10000):
-data = session.generate_round(
-    episode_length=episode_length,
-    num_candidates=num_candidates,
-    temperature=temperature,
-    explore_eps=0.0,
-    # executor=executor,
-    # render=False)
-    render=True)
-
-missing_connections = env.compute_missing_connections()
-# print(env.part_room_id[env.missing_connection_src])
-# print(missing_connections)
-
-# print(torch.stack([env.part_room_id[env.missing_connection_src], missing_connections[0, :].to(torch.long)]))
-print(torch.stack([env.part_room_id[env.missing_connection_src], missing_connections[0, :].to(torch.long)])[:, 15])
-# reward = data[0]
-#     reward = session.envs[0].reward()
-#     max_reward, max_reward_ind = torch.max(reward, dim=0)
-#     num_passes = torch.sum(data.action == len(rooms))
-#     logging.info("{}: doors={}, rooms={}".format(i, max_possible_reward - max_reward, num_passes))
-#     # logging.info("{}: {}".format(i, (max_possible_reward - reward).tolist()))
-#     if max_possible_reward - max_reward.item() == 0:
-#         break
-#     # time.sleep(5)
-# session.envs[0].render(max_reward_ind.item())
+# episode_length = len(rooms)
+# session.env = None
+# session.envs = [env]
+# num_candidates = 44
+# temperature = 0.002
+# torch.manual_seed(0)
+# max_possible_reward = env.max_reward
+# start_time = time.perf_counter()
+# # executor = concurrent.futures.ThreadPoolExecutor(1)
+# # for i in range(10000):
+# data = session.generate_round(
+#     episode_length=episode_length,
+#     num_candidates=num_candidates,
+#     temperature=temperature,
+#     explore_eps=0.0,
+#     # executor=executor,
+#     render=False)
+#     # render=True)
 # end_time = time.perf_counter()
 # print(end_time - start_time)
+# print(torch.sum(~session.envs[0].compute_missing_connections()) - 1)
+# print(torch.sum(~session.envs[0].current_door_connects()))
+#
 # #
-# # extra_map_x = 50
-# # extra_map_y = 50
-# # extra_env = MazeBuilderEnv(rooms,
-# #                      map_x=extra_map_x,
-# #                      map_y=extra_map_y,
-# #                      num_envs=1,
-# #                      device=device,
-# #                      must_areas_be_connected=False)
-# # extra_env.room_mask = ~session.envs[0].room_mask
-# # extra_env.room_position_x = torch.randint(extra_map_x, [1, len(rooms) + 1])
-# # extra_env.room_position_y = torch.randint(extra_map_y, [1, len(rooms) + 1])
-# # extra_env.render()
-
-# len([r for r in env.rooms if len(r.door_ids) == 1])
-torch.sort(torch.sum(session.replay_buffer.episode_data.missing_connects, dim=0))
-session.envs[0].part_room_id[session.envs[0].missing_connection_src[9]]
+# # missing_connections = env.compute_missing_connections()
+# # # print(env.part_room_id[env.missing_connection_src])
+# # # print(missing_connections)
+# #
+# # # print(torch.stack([env.part_room_id[env.missing_connection_src], missing_connections[0, :].to(torch.long)]))
+# # print(torch.stack([env.part_room_id[env.missing_connection_src], missing_connections[0, :].to(torch.long)])[:, 15])
+# # # reward = data[0]
+# # #     reward = session.envs[0].reward()
+# # #     max_reward, max_reward_ind = torch.max(reward, dim=0)
+# # #     num_passes = torch.sum(data.action == len(rooms))
+# # #     logging.info("{}: doors={}, rooms={}".format(i, max_possible_reward - max_reward, num_passes))
+# # #     # logging.info("{}: {}".format(i, (max_possible_reward - reward).tolist()))
+# # #     if max_possible_reward - max_reward.item() == 0:
+# # #         break
+# # #     # time.sleep(5)
+# # # session.envs[0].render(max_reward_ind.item())
+# # # end_time = time.perf_counter()
+# # # print(end_time - start_time)
+# # # #
+# # # # extra_map_x = 50
+# # # # extra_map_y = 50
+# # # # extra_env = MazeBuilderEnv(rooms,
+# # # #                      map_x=extra_map_x,
+# # # #                      map_y=extra_map_y,
+# # # #                      num_envs=1,
+# # # #                      device=device,
+# # # #                      must_areas_be_connected=False)
+# # # # extra_env.room_mask = ~session.envs[0].room_mask
+# # # # extra_env.room_position_x = torch.randint(extra_map_x, [1, len(rooms) + 1])
+# # # # extra_env.room_position_y = torch.randint(extra_map_y, [1, len(rooms) + 1])
+# # # # extra_env.render()
+# #
+# # # len([r for r in env.rooms if len(r.door_ids) == 1])
+# # torch.sort(torch.sum(session.replay_buffer.episode_data.missing_connects, dim=0))
+# # session.envs[0].part_room_id[session.envs[0].missing_connection_src[9]]
