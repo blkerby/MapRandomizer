d45435796ed933952921ec8785c623b3cf6cefdc
Brent Kerby <blkerby@gmail.com>
2022-05-10 18:16:01 -0600

    messing around with tuning model

--- maze_builder/train.py
+++ maze_builder/train.py
@@ -29,8 +29,8 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 start_time = datetime.now()
 pickle_name = 'models/session-{}.pkl'.format(start_time.isoformat())
 
-devices = [torch.device('cpu')]
-# devices = [torch.device('cuda:1'), torch.device('cuda:0')]
+# devices = [torch.device('cpu')]
+devices = [torch.device('cuda:1'), torch.device('cuda:0')]
 num_devices = len(devices)
 device = devices[0]
 executor = concurrent.futures.ThreadPoolExecutor(len(devices))
@@ -86,7 +86,7 @@ logging.info("max_possible_reward = {}".format(max_possible_reward))
 # logging.info("{}".format(model))
 # logging.info("{}".format(optimizer))
 #
-# replay_size = 2 ** 21
+# replay_size = 2 ** 17
 # session = TrainingSession(envs,
 #                           model=model,
 #                           optimizer=optimizer,
@@ -95,23 +95,34 @@ logging.info("max_possible_reward = {}".format(max_possible_reward))
 #                           decay_amount=0.0,
 #                           sam_scale=None)
 # torch.set_printoptions(linewidth=120, threshold=10000)
-#
-# gen_print_freq = 1
-# # gen_print_freq = 16
+# #
+# num_candidates = 1
+# temperature = 1e-10
+# explore_eps = 0.0
+# # gen_print_freq = 1
+# gen_print_freq = 4
 # i = 0
 # total_reward = 0
 # total_reward2 = 0
 # cnt_episodes = 0
+# logging.info("Generating data")
 # while session.replay_buffer.size < session.replay_buffer.capacity:
+#     # data = session.generate_round(
+#     #     episode_length=episode_length,
+#     #     num_candidates=1,
+#     #     temperature=1e-10,
+#     #     # num_candidates=32,
+#     #     # temperature=1e-4,
+#     #     explore_eps=0.0,
+#     #     render=False,
+#     #     executor=executor)
 #     data = session.generate_round(
 #         episode_length=episode_length,
-#         num_candidates=1,
-#         temperature=1e-10,
-#         # num_candidates=32,
-#         # temperature=1e-4,
-#         explore_eps=0.0,
-#         render=False,
-#         executor=executor)
+#         num_candidates=num_candidates,
+#         temperature=temperature,
+#         explore_eps=explore_eps,
+#         executor=executor,
+#         render=False)
 #     session.replay_buffer.insert(data)
 #
 #     total_reward += torch.sum(data.reward.to(torch.float32)).item()
@@ -126,78 +137,74 @@ logging.info("max_possible_reward = {}".format(max_possible_reward))
 #         logging.info("init gen {}/{}: cost={:.3f} +/- {:.3f}".format(
 #             session.replay_buffer.size, session.replay_buffer.capacity,
 #                max_possible_reward - mean_reward, ci_reward))
-#
+
 # pickle.dump(session, open('models/init_session.pkl', 'wb'))
 # pickle.dump(session, open('models/init_session_eval.pkl', 'wb'))
-session = pickle.load(open('models/init_session.pkl', 'rb'))
+# pickle.dump(session, open('models/init_session_eval.pkl', 'wb'))
 
-session_eval = pickle.load(open('models/init_session_eval.pkl', 'rb'))
-eval_batch_size = 256
-eval_num_batches = 4
+# session_eval = pickle.load(open('models/init_session_eval.pkl', 'rb'))
+# session_eval = pickle.load(open('models/checkpoint-1-eval.pkl', 'rb'))
 # eval_batch_size = 8192
-# eval_num_batches = 64
-eval_batches = []
-for i in range(eval_num_batches):
-    logging.info("Generating eval batch {} of size {}".format(i, eval_batch_size))
-    data = session_eval.replay_buffer.sample(eval_batch_size, device=device)
-    eval_batches.append(data)
-pickle.dump(eval_batches, open('models/eval_batches.pkl', 'wb'))
-eval_batches = pickle.load(open('models/eval_batches.pkl', 'rb'))
+# eval_num_batches = 32
+# eval_batches = []
+# for i in range(eval_num_batches):
+#     logging.info("Generating eval batch {} of size {}".format(i, eval_batch_size))
+#     data = session_eval.replay_buffer.sample(eval_batch_size, device=device)
+#     eval_batches.append(data)
+# pickle.dump(eval_batches, open('models/checkpoint-1-eval_batches.pkl', 'wb'))
+# pickle.dump(eval_batches, open('models/eval_batches.pkl', 'wb'))
 
-# session.model = Model(
+# session = pickle.load(open('models/init_session.pkl', 'rb'))
+# eval_batches = pickle.load(open('models/eval_batches.pkl', 'rb'))
+session = pickle.load(open('models/checkpoint-1-train.pkl', 'rb'))
+eval_batches = pickle.load(open('models/checkpoint-1-eval_batches.pkl', 'rb'))
+
+# session.model = DoorLocalModel(
 #     env_config=env_config,
 #     num_doors=envs[0].num_doors,
 #     num_missing_connects=envs[0].num_missing_connects,
 #     num_room_parts=len(envs[0].good_room_parts),
+#     map_channels=4,
+#     map_kernel_size=16,
+#     connectivity_in_width=64,
+#     local_widths=[256, 0],
+#     global_widths=[256, 256],
+#     fc_widths=[256, 256, 256],
+#     alpha=2.0,
 #     arity=2,
-#     map_channels=[16, 64, 256],
-#     map_stride=[2, 2, 2],
-#     map_kernel_size=[7, 5, 3],
-#     map_padding=3 * [False],
-#     room_embedding_width=None,
-#     connectivity_in_width=16,
-#     connectivity_out_width=64,
-#     fc_widths=[256, 256],
-#     global_dropout_p=0.0,
 # ).to(device)
-
-session.model = DoorLocalModel(
-    env_config=env_config,
-    num_doors=envs[0].num_doors,
-    num_missing_connects=envs[0].num_missing_connects,
-    num_room_parts=len(envs[0].good_room_parts),
-    map_channels=4,
-    map_kernel_size=16,
-    connectivity_in_width=64,
-    local_widths=[256, 0],
-    global_widths=[256, 256],
-    fc_widths=[256, 256, 256],
-    alpha=2.0,
-    arity=2,
-).to(device)
-
-session.model.state_value_lin.weight.data.zero_()
-session.model.state_value_lin.bias.data.zero_()
-session.average_parameters = ExponentialAverage(session.model.all_param_data(), beta=session.average_parameters.beta)
-session.optimizer = torch.optim.Adam(session.model.parameters(), lr=0.0001, betas=(0.95, 0.99), eps=1e-8)
-# session.verbose = False
-# # session.replay_buffer.resize(2 ** 21)
+#
+# session.model.state_value_lin.weight.data.zero_()
+# session.model.state_value_lin.bias.data.zero_()
+# session.average_parameters = ExponentialAverage(session.model.all_param_data(), beta=session.average_parameters.beta)
+# session.optimizer = torch.optim.Adam(session.model.parameters(), lr=0.0001, betas=(0.9, 0.9), eps=1e-5)
+# # session.optimizer = torch.optim.RMSprop(session.model.parameters(), lr=0.0004, alpha=0.8, eps=1e-5)
+# # session.verbose = False
+# # # session.replay_buffer.resize(2 ** 21)
 logging.info(session.model)
 logging.info(session.optimizer)
 num_params = sum(torch.prod(torch.tensor(list(param.shape))) for param in session.model.parameters())
 
 
-logging.info("Initial training: {} parameters".format(num_params))
+session.replay_buffer.resize(2 ** 19)
+logging.info("Initial training: {} parameters, {} training examples".format(num_params, session.replay_buffer.size))
 total_loss = 0.0
 total_loss_cnt = 0
 train_round = 1
-train_print_freq = 1  #2048
-batch_size = 512
-train_annealing_time = 200000
-lr0 = 0.0005
-lr1 = 0.00002
-session.decay_amount = 0.1
-session.average_parameters.beta = 0.9
+train_print_freq = 1024
+batch_size = 1024
+# train_annealing_time = 2 ** 16
+train_annealing_time = 1
+lr0 = 0.0004
+lr1 = lr0
+# lr1 = 0.00002
+session.decay_amount = 0.05
+session.average_parameters.beta = 0.9998
+session.optimizer.param_groups[0]['betas'] = (0.9, 0.9)
+session.optimizer.param_groups[0]['eps'] = 1e-5
+logging.info(session.optimizer)
+logging.info("batch_size={}, lr0={}, lr1={}, time={}, decay={}, ema_beta={}".format(
+    batch_size, lr0, lr1, train_annealing_time, session.decay_amount, session.average_parameters.beta))
 for i in range(10000000):
     frac = max(0, min(1, train_round / train_annealing_time))
     lr = lr0 * (lr1 / lr0) ** frac
@@ -205,9 +212,10 @@ for i in range(10000000):
 
     data = session.replay_buffer.sample(batch_size, device=device)
     with util.DelayedKeyboardInterrupt():
-        avg_eval_loss = session.eval_batch(data)
-        total_loss += session.train_batch(data)
-        total_loss_cnt += 1
+        batch_loss = session.train_batch(data)
+        if not math.isnan(batch_loss):
+            total_loss += batch_loss
+            total_loss_cnt += 1
 
     if train_round % train_print_freq == 0:
         avg_loss = total_loss / total_loss_cnt
@@ -215,34 +223,49 @@ for i in range(10000000):
         total_loss_cnt = 0
 
         total_eval_loss = 0.0
+        # logging.info("Computing eval")
         with torch.no_grad():
             with session.average_parameters.average_parameters(session.model.all_param_data()):
-                avg_eval_loss = session.eval_batch(data)
-        #     for eval_data in eval_batches:
-        #         total_eval_loss += session.eval_batch(eval_data)
-        # avg_eval_loss = total_eval_loss / len(eval_batches)
+                for eval_data in eval_batches:
+                    total_eval_loss += session.eval_batch(eval_data)
+        avg_eval_loss = total_eval_loss / len(eval_batches)
 
-        logging.info("init train {}: loss={:.6f}, eval={:.6f}, lr={:.6f}".format(train_round, avg_loss, avg_eval_loss, lr))
+        logging.info("init train {}: loss={:.6f}, eval={:.6f}, frac={:.5f}".format(train_round, avg_loss, avg_eval_loss, frac))
     train_round += 1
 
-pickle.dump(session, open('models/init_train.pkl', 'wb'))
 
 
 
-batch_size_pow0 = 11
-batch_size_pow1 = 11
-lr0 = 5e-5
-lr1 = 5e-5
-num_candidates0 = 40
-num_candidates1 = 40
+
+
+
+
+
+
+
+
+
+
+
+
+# pickle.dump(session, open('models/init_train.pkl', 'wb'))
+# pickle.dump(session, open('models/checkpoint-1-eval.pkl', 'wb'))
+# session = pickle.load(open('models/init_train.pkl', 'rb'))
+
+batch_size_pow0 = 10
+batch_size_pow1 = 10
+lr0 = 0.0005
+lr1 = 0.0005
+num_candidates0 = 8
+num_candidates1 = 16
 num_candidates = num_candidates0
-temperature0 = 5.0
-temperature1 = 2.0
+temperature0 = 100.0
+temperature1 = 10.0
 explore_eps0 = 0.0001
 explore_eps1 = 0.0001
-annealing_start = 34288
-annealing_time = 8192
-pass_factor = 2.0
+annealing_start = 5632
+annealing_time = 4096
+pass_factor = 1.0
 num_gen_rounds = 1
 alpha0 = 0.2
 alpha1 = 0.2
@@ -256,8 +279,8 @@ total_round_cnt = 0
 save_freq = 256
 summary_freq = 512
 session.decay_amount = 0.05
-session.optimizer.param_groups[0]['betas'] = (0.95, 0.99)
-session.average_parameters.beta = 0.9999
+session.optimizer.param_groups[0]['betas'] = (0.9, 0.9)
+session.average_parameters.beta = 0.9998
 
 min_door_value = max_possible_reward
 total_min_door_frac = 0
@@ -376,7 +399,8 @@ for i in range(1000000):
             # episode_data = session.replay_buffer.episode_data
             # session.replay_buffer.episode_data = None
             pickle.dump(session, open(pickle_name, 'wb'))
-            # pickle.dump(session, open(pickle_name + '-b-bk6', 'wb'))
+            # pickle.dump(session, open('models/checkpoint-1-train.pkl', 'wb'))
+            # pickle.dump(session, open(pickle_name + '-bk1', 'wb'))
             # # # # # # # # # session.replay_buffer.episode_data = episode_data
             # session = pickle.load(open(pickle_name + '-bk10', 'rb'))
     if session.num_rounds % summary_freq < num_gen_rounds:
