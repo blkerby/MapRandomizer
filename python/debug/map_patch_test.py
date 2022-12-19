ae73b4657231434d22d145bd880c8129e3c91764
Brent Kerby <blkerby@gmail.com>
2022-12-19 16:17:25 -0700

    changes for v8

--- python/debug/map_patch_test.py
+++ python/debug/map_patch_test.py
@@ -36,8 +36,10 @@ class Rom:
         file = open(filename, 'wb')
         file.write(self.byte_buf)
 
+snes2pc = lambda address: address >> 1 & 0x3F8000 | address & 0x7FFF
 
 input_rom_path = '/home/kerby/Downloads/Super Metroid (JU) [!].smc'
+# input_rom_path = '/home/kerby/Downloads/smmr-v8-66-115673117270825932886574167490559/smmr-v8-66-115673117270825932886574167490559.sfc'
 # input_rom_path = '/home/kerby/Downloads/smmr-v0-30-115673117270825932886574167490559.sfc'
 # input_rom_path = '/home/kerby/Downloads/smmr-v0-5-115673117270825932886574167490559.sfc'
 output_rom_path = '/home/kerby/Downloads/maptest.smc'
@@ -46,48 +48,59 @@ rom = Rom(open(input_rom_path, 'rb'))
 patches = [
     'new_game_extra',
     # 'new_game',
-    'disable_map_icons',
-    'tourian_map',
-    'crateria_sky_fixed',
+    # 'disable_map_icons',
+    # 'tourian_map',
+    # 'crateria_sky_fixed',
     # 'no_map_select'
+    # 'escape_room_1',
 ]
 for patch_name in patches:
     patch = ips_util.Patch.load('patches/ips/{}.ips'.format(patch_name))
     rom.byte_buf = patch.apply(rom.byte_buf)
 
-area_arr = [rom.read_u8(room.rom_address + 1) for room in rooms]
-apply_map_patches(rom, area_arr)
-
-# Messing around with removing the bottom part of the pause menu, since these occupy a lot of tiles that we
-# might want to repurpose for something more useful (e.g. showing door locations on the map). Looks funny though:
-# n = 0x1C0
-# rom.write_n(snes2pc(0xB6E640), n, (n // 2) * [0x00, 0x00])
+# Remove gray lock on Climb left door:
+ptr = snes2pc(0x8F830C - 14)
+rom.write_u16(ptr, 0xB63B)  # right continuation arrow (should have no effect, giving a blue door)
+rom.write_u16(ptr + 2, 0)  # position = (0, 0)
 #
-snes2pc = lambda address: address >> 1 & 0x3F8000 | address & 0x7FFF
+# area_arr = [rom.read_u8(room.rom_address + 1) for room in rooms]
+# apply_map_patches(rom, area_arr)
+#
+# # Messing around with removing the bottom part of the pause menu, since these occupy a lot of tiles that we
+# # might want to repurpose for something more useful (e.g. showing door locations on the map). Looks funny though:
+# # n = 0x1C0
+# # rom.write_n(snes2pc(0xB6E640), n, (n // 2) * [0x00, 0x00])
+# #
+#
+# # import numpy as np
+# # image = np.zeros([128, 128])
+# # for i in range(256):
+# #     data = read_tile_2bpp(rom, snes2pc(0x9AB200), i)
+# #     x = i // 16
+# #     y = i % 16
+# #     x0 = x * 8
+# #     x1 = (x + 1) * 8
+# #     y0 = y * 8
+# #     y1 = (y + 1) * 8
+# #     image[x0:x1, y0:y1] = data
+# #     # for row in data:
+# #     #     print(''.join('{:x}'.format(x) for x in row))
+# #     # data = read_tile_4bpp(rom, snes2pc(0xB68000), i)
+# #     # for row in data:
+# #     #     print(''.join('{:x}'.format(x) for x in row))
+# # from matplotlib import pyplot as plt
+# # plt.imshow(image)
+#
+# # rom.write_u16(snes2pc(0x819124), 0x0009)   # File select index 9 - load
+#
+# # Skip map screens when starting after game over
+# # rom.write_u16(snes2pc(0x81911F), 0x0006)
+#
 
-# import numpy as np
-# image = np.zeros([128, 128])
-# for i in range(256):
-#     data = read_tile_2bpp(rom, snes2pc(0x9AB200), i)
-#     x = i // 16
-#     y = i % 16
-#     x0 = x * 8
-#     x1 = (x + 1) * 8
-#     y0 = y * 8
-#     y1 = (y + 1) * 8
-#     image[x0:x1, y0:y1] = data
-#     # for row in data:
-#     #     print(''.join('{:x}'.format(x) for x in row))
-#     # data = read_tile_4bpp(rom, snes2pc(0xB68000), i)
-#     # for row in data:
-#     #     print(''.join('{:x}'.format(x) for x in row))
-# from matplotlib import pyplot as plt
-# plt.imshow(image)
-
-# rom.write_u16(snes2pc(0x819124), 0x0009)   # File select index 9 - load
-
-# Skip map screens when starting after game over
-# rom.write_u16(snes2pc(0x81911F), 0x0006)
+# for i in range(12):
+#     b = rom.read_u8(snes2pc(0x838060 + i))
+#     print("{:x}".format(b))
+print("{:x}".format(rom.read_u16(snes2pc(0x8FC87B))))
 
 rom.save(output_rom_path)
 os.system(f"rm {output_rom_path[:-4]}.srm")
