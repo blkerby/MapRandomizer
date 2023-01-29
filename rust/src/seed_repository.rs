92d63ed02244379e57e3f0866e1b01b423e52b95
Brent Kerby <blkerby@gmail.com>
2023-01-29 14:54:32 -0700

    Split website into two pages, to allow users to share generated seeds

--- rust/src/seed_repository.rs
+++ rust/src/seed_repository.rs
@@ -1,68 +1,88 @@
-use actix_web::{web::Bytes, body::MessageBody};
-use object_store::{gcp::GoogleCloudStorageBuilder, ObjectStore};
+use std::path::Path;
+
+use actix_web::{body::MessageBody, web::Bytes};
 use anyhow::Result;
 use log::info;
+use object_store::{gcp::GoogleCloudStorageBuilder, ObjectStore, memory::InMemory, local::LocalFileSystem};
 
 // Data needed to render the web page for a randomized seed and to use it to patch a ROM.
-// The fields `patch_ips` and `seed_html` correspond to the mandatory files "patch.ips" and
-// "seed.html". Extra files (e.g. spoiler logs/maps) can also be added which may be referenced
-// in "seed.html". We deliberately use a generic, minimally structured format here, to ensure
-// compatibility across Map Rando versions. Each of these files is stored in durable object
-// storage (currently Google Cloud Storage) to allow players to share seeds.
+// The files `patch.ips`, `seed_footer.html`, and `seed_header.html` are mandatory,
+// Extra files (e.g. spoiler logs/maps) can also be added which may be referenced
+// in `seed_footer.html` and/or `seed_header`. We deliberately use a generic, minimally 
+// structured format here, to ensure compatibility across Map Rando versions. Each of 
+// these files is stored in durable object storage (currently Google Cloud Storage) 
+// to allow players to share seeds.
 pub struct Seed {
     pub name: String,
-    pub patch_ips: Vec<u8>,
-    pub seed_html: Vec<u8>,
-    pub extra_files: Vec<SeedExtraFile>,
+    pub files: Vec<SeedFile>,
 }
 
-pub struct SeedExtraFile {
+pub struct SeedFile {
     filename: String,
     data: Vec<u8>,
 }
 
-pub struct SeedRepository<'a> {
-    object_store: Box<dyn ObjectStore>,
-    base_path: String,
-    zstd_compressor: zstd::bulk::Compressor<'a>,
-    zstd_decompressor: zstd::bulk::Decompressor<'a>,
+impl SeedFile {
+    pub fn new(filename: &str, data: Vec<u8>) -> Self {
+        SeedFile {
+            filename: filename.to_string(),
+            data
+        }
+    }
 }
 
-impl<'a> SeedRepository<'a> {
+pub struct SeedRepository {
+    object_store: Box<dyn ObjectStore>,
+    base_path: String,
+}
+
+impl SeedRepository {
     pub fn new(url: &str) -> Result<Self> {
+        let object_store: Box<dyn ObjectStore> = if url.starts_with("gs:") {
+            Box::new(
+                    GoogleCloudStorageBuilder::from_env()
+                        .with_url(url)
+                        .build()?)
+        } else if url == "mem" {
+            Box::new(InMemory::new())
+        } else if url.starts_with("file:") {
+            let root = &url[5..];
+            Box::new(LocalFileSystem::new_with_prefix(Path::new(root))?)
+        } else {
+            panic!("Unsupported seed repository type: {}", url);
+        };
         Ok(Self {
-            object_store: Box::new(
-                GoogleCloudStorageBuilder::from_env()
-                    .with_url(url)
-                    .build()?,
-            ),
+            object_store,
             base_path: "seeds/".to_string(),
-            zstd_compressor: zstd::bulk::Compressor::new(15)?,
-            zstd_decompressor: zstd::bulk::Decompressor::new()?,
-        })
+        })    
     }
 
-    pub async fn get_file(&mut self, seed_name: &str, filename: &str) -> Result<Bytes> {
-        let path = object_store::path::Path::parse(self.base_path.clone() + seed_name + "/" + filename)?;
+    pub async fn get_file(&self, seed_name: &str, filename: &str) -> Result<Bytes> {
+        let path = object_store::path::Path::parse(
+            self.base_path.clone() + seed_name + "/" + filename + ".zstd",
+        )?;
         let compressed_data = self.object_store.get(&path).await?.bytes().await?;
-        let data = self.zstd_decompressor.decompress(&compressed_data, 10_000_000)?;
+        let data = zstd::bulk::decompress(&compressed_data, 10_000_000)?;
         Ok(data.try_into_bytes().unwrap())
     }
 
-    pub async fn put_file(&mut self, seed_name: &str, filename: String, data: Vec<u8>) -> Result<()> {
-        let path = object_store::path::Path::parse(self.base_path.clone() + seed_name + "/" + filename.as_str())?;
-        let compressed_data = self.zstd_compressor.compress(&data)?;
+    pub async fn put_file(&self, seed_name: &str, filename: String, data: Vec<u8>) -> Result<()> {
+        let path = object_store::path::Path::parse(
+            self.base_path.clone() + seed_name + "/" + filename.as_str() + ".zstd",
+        )?;
+        // info!("Compressing {}", filename);
+        let compressed_data = zstd::bulk::compress(&data, 15)?;
+        // info!("Writing {}", filename);
         self.object_store.put(&path, compressed_data.into()).await?;
+        // info!("Done with {}", filename);
         Ok(())
     }
 
-    pub async fn put_seed(&mut self, seed: Seed) -> Result<()> {
+    pub async fn put_seed(&self, seed: Seed) -> Result<()> {
         info!("Storing seed");
         let mut futures = Vec::new();
-        futures.push(self.put_file(&seed.name, "patch.ips".to_string(), seed.patch_ips));
-        futures.push(self.put_file(&seed.name, "seed.html".to_string(), seed.seed_html));
-        for extra in seed.extra_files {
-            futures.push(self.put_file(&seed.name, extra.filename, extra.data));
+        for file in seed.files {
+            futures.push(self.put_file(&seed.name, file.filename, file.data));
         }
         let results = futures::future::join_all(futures).await;
         for result in results {
