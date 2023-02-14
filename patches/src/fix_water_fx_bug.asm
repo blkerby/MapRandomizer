fa638a73ece587c9ee0defb1a37ab35b56e1d549
Brent Kerby <blkerby@gmail.com>
2023-02-14 16:58:41 -0700

    add fix_water_fx_bug IPS and ASM

---
+++ patches/src/fix_water_fx_bug.asm
@@ -0,0 +1,15 @@
+; Patch from Benox (via dagit and moehr)
+
+org $82E659
+    JSR handle_door_transition
+    NOP
+
+    ;Do stuff
+    org $82F880
+handle_door_transition:
+
+    ; Lets fix the entering from water to no water animation bug 
+    STZ $0A9C
+
+    JSL $878064 ; run hi-jacked instruction
+    RTS
