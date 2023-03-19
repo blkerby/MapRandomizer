92317cfb89988e242b916216f094aeb0f184c042
Brent Kerby <blkerby@gmail.com>
2023-03-18 21:41:56 -0600

    continuing work on boss doors

--- patches/src/gray_doors.asm
+++ patches/src/gray_doors.asm
@@ -28,6 +28,7 @@ bt_door_left:
 .wait_clear
     dw $0002, $A683    ; Wait for Samus not to be in the doorway (to avoid getting stuck)
     dw left_doorway_clear, .wait_clear  
+.closing
     dw $8C19        ; Queue sound 8, sound library 3, max queued sounds allowed = 6 (door closed)
     db $08    
     dw $0002, $A6FB
@@ -49,11 +50,54 @@ item_collect:
     ora $05e7 		; original hijacked code
     rts
 
+check_area_boss:
+    lda #$0001
+    jsl $8081DC
+    rts
+
+check_area_miniboss:
+    lda #$0002
+    jsl $8081DC
+    rts
+
+check_area_torizo:
+    lda #$0004
+    jsl $8081DC
+    rts
+
+check_enemy_quota:
+    lda $0E50
+    cmp $0E52
+    rts
+
+check_list:
+    dw check_area_boss, check_area_miniboss, check_area_torizo, check_enemy_quota
+
 ;;; check if we the BT door condition is triggered (item collected, or boss hurt)
 ;;; if triggered: set zero flag
 btcheck:
     lda !BTRoomFlag
     cmp !PickedUp
+    beq .done
+;    jsr check_area_boss
+    
+    ;phy
+    ;lda check_list, y
+    ;ldx #check_area_boss
+    ;phx
+    ;tax
+    phx
+    lda $1E17, x
+    tax
+    jsr (check_list, x)
+    plx
+    ;plx
+    ;ply
+
+    lda #$ffff  ;\ transfer carry flag to zero flag
+    adc #$0000  ;/
+
+.done
     rts
 
 ;;; check if we the BT door condition is triggered (item collected, or boss hurt)
@@ -109,14 +153,15 @@ handle_door_transition:
 
 ; Right-side Bomb-Torizo-type door
 right_bt_door:
-    dw $C794, btdoor_right, btdoor_setup_right
+;    dw $C794, btdoor_right, btdoor_setup_right
+    dw $C794, $BE70, btdoor_setup_right
 ;    dw $C794, $BA7F, $BA4C
 
 
 btdoor_setup_right:
 .wait_trigger
     dw $0002, $A677
-;    dw btcheck_inst, .wait_trigger  ; Go to .wait_trigger unless the condition is triggered (item collected or boss hurt)
+    dw btcheck_inst, .wait_trigger  ; Go to .wait_trigger unless the condition is triggered (item collected or boss hurt)
     dw $0028, $A677    ; After the condition is triggered, wait a bit before closing
 .wait_clear
     dw $0002, $A677    ; Wait for Samus not to be in the doorway (to avoid getting stuck)
@@ -127,39 +172,40 @@ btdoor_setup_right:
     dw $0002,$A6BF
     dw $0002,$A6B3
     dw $0001,$A6A7
-    dw $8724,btdoor_right   ; Go to btdoor_right
+;    dw $8724,btdoor_right
+    dw $8724, $BE70
 
-btdoor_right:
-    dw $8A72, $C4B1      ; Go to $C4B1 (blue door) if the room argument door is set
-    dw $8A24, .wait      ; Link instruction = .wait
-    dw $BE3F           ; Set grey door pre-instruction
-    dw $0001, $A6A7
-.sleep
-    dw $86B4            ; Sleep
-    dw $8724, .sleep      ; Go to .sleep
-.wait
-    dw $8A24, .shot      ; Link instruction = .shot
-    dw $86C1, $BD0F      ; Pre-instruction = go to link instruction if shot
-.flashing
-    dw $0003, $A9B3
-    dw $0004, $A6A7
-    dw $0003, $A9B3
-    dw $0004, $A6A7
-    dw $0003, $A9B3
-    dw $0004, $A6A7
-    dw $8724, .flashing      ; Go to .flashing
-.shot
-    dw $8A91
-    db $01
-    dw .opening   ; Increment door hit counter; Set room argument door and go to $BABC if [door hit counter] >= 01h
-.opening             
-    dw $8C19
-    db $07        ; Queue sound 7, sound library 3, max queued sounds allowed = 6 (door opened)
-    dw $0004, $A6B3
-    dw $0004, $A6BF
-    dw $0004, $A6CB
-    dw $0001, $A677
-    dw $86BC
+;btdoor_right:
+;    dw $8A72, $C4B1      ; Go to $C4B1 (blue door) if the room argument door is set
+;    dw $8A24, .wait      ; Link instruction = .wait
+;    dw $BE3F           ; Set grey door pre-instruction
+;    dw $0001, $A6A7
+;.sleep
+;    dw $86B4            ; Sleep
+;    dw $8724, .sleep      ; Go to .sleep
+;.wait
+;    dw $8A24, .shot      ; Link instruction = .shot
+;    dw $86C1, $BD0F      ; Pre-instruction = go to link instruction if shot
+;.flashing
+;    dw $0003, $A9B3
+;    dw $0004, $A6A7
+;    dw $0003, $A9B3
+;    dw $0004, $A6A7
+;    dw $0003, $A9B3
+;    dw $0004, $A6A7
+;    dw $8724, .flashing      ; Go to .flashing
+;.shot
+;    dw $8A91
+;    db $01
+;    dw .opening   ; Increment door hit counter; Set room argument door and go to $BABC if [door hit counter] >= 01h
+;.opening             
+;    dw $8C19
+;    db $07        ; Queue sound 7, sound library 3, max queued sounds allowed = 6 (door opened)
+;    dw $0004, $A6B3
+;    dw $0004, $A6BF
+;    dw $0004, $A6CB
+;    dw $0001, $A677
+;    dw $86BC
 
 warnpc $84f980
 ; FIX ME: make these patches more compact (reuse vanilla instruction lists more?)
@@ -183,9 +229,9 @@ bt_instr:
 org $8FC553
     dw $BAF4
 
-; Override door PLM for Golden Torizo right door
-org $8F8E7A
-    dw right_bt_door
+;; Override door PLM for Golden Torizo right door
+;org $8F8E7A
+;    dw right_bt_door
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -213,6 +259,9 @@ org $8398D4   ; door ASM for entering Ridley's Room from the right
 
 org $839A90   ; door ASM for entering Golden Torizo's Room from the right
     dw make_right_doors_bt
+
+org $83A77E   ; door ASM for entering Botwoon's Room from the left
+    dw make_left_doors_bt
 
 org $839184   ; door ASM for entering Baby Kraid Room from the left
     dw make_left_doors_blue
@@ -269,7 +318,6 @@ make_right_doors_blue:
 make_right_doors_bt:
     lda #$C842 : sta $00  ; right doors
     lda #right_bt_door : sta $02  ; BT-type door
-    ;lda #$BAF4 : sta $02  ; BT-type door
     jmp change_doors
 
 
@@ -295,19 +343,23 @@ warnpc $8FF800
 ;;;;;;;;
 
 org $A7DD42
-    jsl phantoon_shot
+    jsl phantoon_hurt
     nop : nop
 
 org $A7B374
-    jsl kraid_shot
+    jsl kraid_hurt
 
 org $A5954D
-    jsl draygon_shot
+    jsl draygon_hurt
+    nop : nop
+
+org $A6B297
+    jsl ridley_hurt
     nop : nop
 
 ; free space in bank $A0
 org $A0F7D3
-phantoon_shot:
+phantoon_hurt:
     lda !PickedUp
     sta !BTRoomFlag
     ; run hi-jacked instructions
@@ -315,17 +367,25 @@ phantoon_shot:
     cmp #$0008
     rtl
 
-kraid_shot:
+kraid_hurt:
     lda !PickedUp
     sta !BTRoomFlag
     ; run hi-jacked instruction
     lda $7E782A
     rtl
 
-draygon_shot:
+draygon_hurt:
     lda !PickedUp
     sta !BTRoomFlag
     ; run hi-jacked instruction
     ldy #$A277
     ldx $0E54
+    rtl
+
+ridley_hurt:
+    lda !PickedUp
+    sta !BTRoomFlag
+    ; run hi-jacked instruction
+    lda $0FA4
+    and #$0001
     rtl
--- python/debug/map_patch_test.py
+++ python/debug/map_patch_test.py
@@ -7,7 +7,8 @@ import io
 import os
 
 
-input_rom_path = '/home/kerby/Downloads/Super Metroid (JU) [!].smc'
+# input_rom_path = '/home/kerby/Downloads/Super Metroid (JU) [!].smc'
+input_rom_path = '/home/kerby/Downloads/Practice-v2.5.1-suits.sfc'
 # input_rom_path = '/home/kerby/Downloads/smmr-v8-66-115673117270825932886574167490559/smmr-v8-66-115673117270825932886574167490559.sfc'
 # input_rom_path = '/home/kerby/Downloads/smmr-v0-30-115673117270825932886574167490559.sfc'
 # input_rom_path = '/home/kerby/Downloads/smmr-v0-5-115673117270825932886574167490559.sfc'
@@ -67,26 +68,29 @@ for patch_name in patches:
     patch = ips_util.Patch.load('patches/ips/{}.ips'.format(patch_name))
     rom.bytes_io = BytesIO(patch.apply(rom.bytes_io.getvalue()))
 
+# release Kraid camera so it won't be as glitched when entering from the right
+rom.write_n(snes2pc(0xA7A9F4), 4, bytes(4 * [0xEA]))
+# No longer restrict Samus X position to left screen during start of Kraid fight
+rom.write_u8(snes2pc(0xA7C9EE), 0x60)
 
-
-map_patcher = MapPatcher(rom, area_arr)
-# for i, idx in enumerate(new_text_tile_idxs):
-#     map_patcher.write_tile_2bpp(idx, vanilla_text_tiles[i], switch_red_white=False)
-
-plm_types_to_remove = [
-    0xC88A, 0xC85A, 0xC872,  # right pink/yellow/green door
-    0xC890, 0xC860, 0xC878,  # left pink/yellow/green door
-    0xC896, 0xC866, 0xC87E,  # down pink/yellow/green door
-    0xC89C, 0xC86C, 0xC884,  # up pink/yellow/green door
-    0xDB48, 0xDB4C, 0xDB52, 0xDB56, 0xDB5A, 0xDB60,  # eye doors
-    0xC8CA,  # wall in Escape Room 1
-]
-gray_door_plm_types = [
-    0xC848,  # left gray door
-    0xC842,  # right gray door
-    0xC854,  # up gray door
-    0xC84E,  # down gray door
-]
+# map_patcher = MapPatcher(rom, area_arr)
+# # for i, idx in enumerate(new_text_tile_idxs):
+# #     map_patcher.write_tile_2bpp(idx, vanilla_text_tiles[i], switch_red_white=False)
+#
+# plm_types_to_remove = [
+#     0xC88A, 0xC85A, 0xC872,  # right pink/yellow/green door
+#     0xC890, 0xC860, 0xC878,  # left pink/yellow/green door
+#     0xC896, 0xC866, 0xC87E,  # down pink/yellow/green door
+#     0xC89C, 0xC86C, 0xC884,  # up pink/yellow/green door
+#     0xDB48, 0xDB4C, 0xDB52, 0xDB56, 0xDB5A, 0xDB60,  # eye doors
+#     0xC8CA,  # wall in Escape Room 1
+# ]
+# gray_door_plm_types = [
+#     0xC848,  # left gray door
+#     0xC842,  # right gray door
+#     0xC854,  # up gray door
+#     0xC84E,  # down gray door
+# ]
 # boss_room_names = [
 #     "Kraid Room",
 #     "Phantoon's Room",
