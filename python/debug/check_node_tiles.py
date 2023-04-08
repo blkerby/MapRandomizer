9480c1aac437cd52192d646612402939fe962450
Brent Kerby <blkerby@gmail.com>
2023-04-07 18:15:23 -0600

    corrections to node_tile data

---
+++ python/debug/check_node_tiles.py
@@ -0,0 +1,22 @@
+from logic.rooms.all_rooms import rooms
+
+for room in rooms:
+    try:
+        all_coords_set = set()
+        for y in range(len(room.map)):
+            for x in range(len(room.map[0])):
+                if room.map[y][x] == 1:
+                    all_coords_set.add((x, y))
+
+        covered_coords_set = set()
+        for k, v in room.node_tiles.items():
+            for (x, y) in v:
+                if (x, y) not in all_coords_set:
+                    print("Room='{}', Node={}, coords=({}, {}): not valid".format(room.name, k, x, y))
+                covered_coords_set.add((x, y))
+
+        for (x, y) in all_coords_set:
+            if (x, y) not in covered_coords_set:
+                print("Room='{}', coords=({}, {}): not covered".format(room.name, x, y))
+    except Exception as e:
+        print("Room='{}': {}".format(room.name, e))
