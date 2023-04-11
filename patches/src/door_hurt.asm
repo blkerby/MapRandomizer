169bc915d1c84516bab3e7a6a4b785ae595e16eb
Brent Kerby <blkerby@gmail.com>
2023-04-10 23:29:31 -0600

    extend door_hurt patch to work through vertical transitions, be conditional on x-ray selected

--- patches/src/door_hurt.asm
+++ patches/src/door_hurt.asm
@@ -7,8 +7,22 @@ org $82E2F7
 ; Free space in bank $82:
 org $82FA00
 door_hurt:
+    jsr $DE12  ; run hi-jacked instruction
+
+    lda #$0005
+    cmp $09D2 ; is x-ray selected?
+    bne .skip ; if not, skip getting hurt through the door
+
     lda #$0005
     sta $18AA  ; set knockback timer to 5
     dec $09C2  ; decrease Samus' energy by 1
-    jsr $DE12  ; run hi-jacked instruction
+    
+    ; extra stuff to make vertical setups work:
+    stz $0B2E  ; set Samus Y speed to 0
+    stz $0B2C  ; set Samus Y subspeed to 0
+    lda #$0000
+    sta $0A11  ; set previous movement type to standing
+    sta $0A1F  ; set movement type to standing
+
+.skip:
     rts
