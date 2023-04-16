59f1b0fccde967097cd3792e129de4f4627df37c
Brent Kerby <blkerby@gmail.com>
2023-04-16 09:18:36 -0600

    revert some changes messing with the model

--- python/maze_builder/model.py
+++ python/maze_builder/model.py
@@ -397,8 +397,7 @@ class DoorLocalModel(torch.nn.Module):
         self.right_lin = torch.nn.Linear(map_kernel_size ** 2 * map_channels, local_widths[0] * arity)
         self.up_lin = torch.nn.Linear(map_kernel_size ** 2 * map_channels, local_widths[0] * arity)
         self.down_lin = torch.nn.Linear(map_kernel_size ** 2 * map_channels, local_widths[0] * arity)
-        # self.global_lin = torch.nn.Linear(connectivity_in_width ** 2 + self.num_rooms + 3, global_widths[0] * arity)
-        self.global_lin = torch.nn.Linear(self.num_rooms + 3, global_widths[0] * arity)
+        self.global_lin = torch.nn.Linear(connectivity_in_width ** 2 + self.num_rooms + 3, global_widths[0] * arity)
         # self.global_lin = torch.nn.Linear(self.num_rooms + 1, global_widths[0] * arity)
         self.base_local_act = common_act
         self.base_global_act = common_act
@@ -479,22 +478,21 @@ class DoorLocalModel(torch.nn.Module):
             local_pos_emb_y = self.pos_embedding_y[local_pos_y, :]
             local_X = local_X + local_pos_emb_x + local_pos_emb_y
 
-            # reduced_connectivity, missing_connects = env.compute_fast_component_matrix_cpu2(
+            reduced_connectivity, missing_connects = env.compute_fast_component_matrix_cpu2(
+                room_mask, room_position_x, room_position_y,
+                self.connectivity_left_mat, self.connectivity_right_mat)
+            # reduced_connectivity, missing_connects = env.compute_fast_component_matrix(
             #     room_mask, room_position_x, room_position_y,
             #     self.connectivity_left_mat, self.connectivity_right_mat)
-            # # reduced_connectivity, missing_connects = env.compute_fast_component_matrix(
-            # #     room_mask, room_position_x, room_position_y,
-            # #     self.connectivity_left_mat, self.connectivity_right_mat)
-            # reduced_connectivity_flat = reduced_connectivity.view(n, self.connectivity_in_width ** 2)
+            reduced_connectivity_flat = reduced_connectivity.view(n, self.connectivity_in_width ** 2)
 
             # global_X = torch.cat([room_mask.to(X_left.dtype),
             #                       steps_remaining.view(-1, 1)], dim=1)
-            global_X = torch.cat([
-                # reduced_connectivity_flat,
-                  room_mask.to(X_left.dtype),
-                  steps_remaining.view(-1, 1) / 100.0,
-                  round_frac.view(-1, 1),
-                  torch.log(temperature.view(-1, 1))], dim=1)
+            global_X = torch.cat([reduced_connectivity_flat,
+                                  room_mask.to(X_left.dtype),
+                                  steps_remaining.view(-1, 1) / 100.0,
+                                  round_frac.view(-1, 1),
+                                  torch.log(temperature.view(-1, 1))], dim=1)
             global_X = self.global_lin(global_X)
             global_X = self.base_global_act(global_X)
 
@@ -549,10 +547,8 @@ class DoorLocalModel(torch.nn.Module):
 
             door_connects_filtered_logodds = torch.where(door_connects, inf_tensor_door, door_connects_raw_logodds)
             # print(missing_connects.shape, inf_tensor_missing.shape, missing_connects_raw_logodds.shape)
-
-            # missing_connects_filtered_logodds = torch.where(missing_connects.to(torch.bool), inf_tensor_missing,
-            #                                                 missing_connects_raw_logodds)
-            missing_connects_filtered_logodds = missing_connects_raw_logodds   # TODO: add back filtering
+            missing_connects_filtered_logodds = torch.where(missing_connects.to(torch.bool), inf_tensor_missing,
+                                                            missing_connects_raw_logodds)
 
             # all_filtered_logodds = torch.cat([door_connects_filtered_logodds, missing_connects_raw_logodds], dim=1)
             all_filtered_logodds = torch.cat([door_connects_filtered_logodds, missing_connects_filtered_logodds], dim=1)
