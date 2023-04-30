6d86f7a21275576d47e21c7094ff382bf8c5b460
Brent Kerby <blkerby@gmail.com>
2023-04-30 11:37:31 -0600

    darken HUD grid lines

--- patches/src/fix_water_fx_bug.asm
+++ patches/src/fix_water_fx_bug.asm
@@ -4,14 +4,12 @@ org $82E659
     JSR handle_door_transition
     NOP
 
-    ;Do stuff
-    org $82F880
+; Free space in bank $82:
+org $82FA80
 handle_door_transition:
-
     ; Lets fix the entering from water to no water animation bug 
     STZ $0A9C
-
-    
-
     JSL $878064 ; run hi-jacked instruction
     RTS
+
+warnpc $82FA90
