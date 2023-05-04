39256d3c6635cdc831e325983f0f5958d1029d82
Brent Kerby <blkerby@gmail.com>
2023-05-03 20:42:02 -0600

    fix bug with not being able to scroll to map station

--- patches/src/new_game_extra.asm
+++ patches/src/new_game_extra.asm
@@ -84,6 +84,19 @@ start_game:
 ;    sta $7ED829
 ;    sta $7ED82B
 
+    ; If there are no existing save files, then clear map revealed tiles (persisted across deaths/reloads)
+    lda $0954
+    bne .skip_clear_revealed
+    ldx #$0600
+.clear_revealed
+    dex
+    dex
+    lda #$0000
+    sta $702000, X    
+    txa
+    bne .clear_revealed
+.skip_clear_revealed:
+
     ; Copy initial explored tiles from B5:F000 (to set map station tiles to explored)
     ; Also initialize these as revealed tiles (so that map station tiles will be taken into account in pause map scroll limits).
     ldx #$0600
@@ -106,19 +119,6 @@ start_game:
     sta $07F7, X
     txa
     bne .copy_explored_crateria
-
-    ; If there are no existing save files, then clear map revealed tiles (persisted across deaths/reloads)
-    lda $0954
-    bne .skip_clear_revealed
-    ldx #$0600
-.clear_revealed
-    dex
-    dex
-    lda #$0000
-    sta $702000, X    
-    txa
-    bne .clear_revealed
-.skip_clear_revealed:
 
     ; Unlock Tourian statues room (to avoid camera glitching when entering from bottom, and also to ensure game is
     ; beatable since we don't take it into account as an obstacle in the item randomization logic)
