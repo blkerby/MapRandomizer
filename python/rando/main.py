b096658c33d75eaa7784c822ca358040e9141be6
Brent Kerby <blkerby@gmail.com>
2023-06-13 14:00:04 -0600

    getting color mode toggler working

--- python/rando/main.py
+++ python/rando/main.py
@@ -531,7 +531,6 @@ def home():
                 </div>
             </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>        
         <script>
         document.getElementById("randomSeed").value = Math.floor(Math.random() * 0x7fffffff);        
         function presetChanged() {{
--- rust/static/colorMode.js
+++ rust/static/colorMode.js
@@ -20,10 +20,14 @@
     }
   
     const setTheme = theme => {
-      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
+      if (theme === 'auto') {
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          document.documentElement.setAttribute('data-bs-theme', 'dark');
+        } else {
+          document.documentElement.setAttribute('data-bs-theme', 'light');
+        }
       } else {
-        document.documentElement.setAttribute('data-bs-theme', theme)
+        document.documentElement.setAttribute('data-bs-theme', theme);
       }
     }
   
