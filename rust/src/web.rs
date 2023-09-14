831797aae3733ba63c0f2ed6b6ea36b11566490e
Brent Kerby <blkerby@gmail.com>
2023-09-14 13:08:44 -0600

    start prepping v86

--- rust/src/web.rs
+++ rust/src/web.rs
@@ -11,7 +11,7 @@ use crate::seed_repository::SeedRepository;
 
 use self::logic::LogicData;
 
-pub const VERSION: usize = 85;
+pub const VERSION: usize = 86;
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Preset {
