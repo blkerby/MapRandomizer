bc485218fa921969706f64c8c98c36ab03eee446
Brent Kerby <blkerby@gmail.com>
2024-01-19 09:15:03 -0700

    first pass at implementing ukkonen's algorithm

--- rust/src/patch/suffix_tree.rs
+++ rust/src/patch/suffix_tree.rs
@@ -1,145 +1,197 @@
 use hashbrown::HashMap;
 
+type DataIdx = u32;
+type NodeId = u32;
+type EdgeId = u8;
+
+const DATA_END: DataIdx = DataIdx::MAX;
+const NODE_UNDEFINED: NodeId = NodeId::MAX;
+const EDGE_UNDEFINED: EdgeId = EdgeId::MAX;  // Note: This is not a distinct value from valid edge IDs.
+
+// We build SuffixTree using Ukkonen's algorithm, appending one byte at a time, starting from an empty data vector.
+// This is a compact way indexing substrings of a data vector (Vec<u8>) in a way that allows efficient searching.
+// Even though there are O(n^2) substrings, the tree structure only occupies O(n) space and takes O(n) time to construct.
+// Every node in the tree represents a substring that occurs in the data (possibly in multiple places); the substring
+// represented by a node is given by concatenating the edge label of all the edges along the path from the root node 
+// down to the given node. Only internal nodes (not leaf nodes) are explicitly represented using a Node struct. 
+// At each stage, the following invariants are satisfied:
+// 1. Every possible substring of the data (i.e. data[i..j] for any i <= j) is represented somewhere in the tree, meaning 
+//    there is a path starting from the root node to another node, such that concatenating the edge labels along the path 
+//    (with possibly just a prefix of the last edge label being used) results in that substring.
+// 2. For every node and every possible byte value, the node has at most one child edge with label starting with that byte value.
+// 3. Every (non-leaf) node has at least two child edges, except for possibly the root node.
+// 4. A special "cut" position is kept track of, such that for all i < cut, the suffix data[i..] does not occur as a 
+//    substring anywhere else in the data (which means it is represented by a leaf).
+// 5. For every (non-leaf) non-root node representing data[i..j] (for i < j), a "suffix link" is maintained, which is a pointer to
+//    the node for data[(i + 1)..j].
 pub struct SuffixTree {
-    pub tree: Tree,
-    pub data: Vec<u8>,
+    data: Vec<u8>,
+    nodes: Vec<Node>,
+    // position of smallest suffix data[i..] such that for all j < i, the substring data[j..] occurs nowhere else in data:
+    // Note we don't track the index "i" explicitly (as it is not needed), only the position in the tree.
+    cut: Position,   
+}
+
+// Identifies a position in the tree by a node and (optionally) a prefix of a leaf child edge belonging to it
+struct Position {
+    node_id: NodeId,
+    edge_id: EdgeId,  // index of child edge below this node (ignored if length == 0).
+    length: DataIdx,  // length of prefix of the child edge label to follow
+}
+
+struct Node {
+    edges: Vec<Edge>,  // Child edges below this node
+    edge_lookup: HashMap<u8, u8>,  // Mapping from first byte of edge label to index in `edges`.
+    suffix_link: NodeId,
+}
+
+struct Edge {
+    // Edge label references data indirectly, so that it takes constant space regardless of its length.
+    // If the edge ends in a leaf (if `node_id == NODE_UNDEFINED`), then it references data[start..].
+    // Otherwise it references data[start..end].
+    start: DataIdx,  // Starting index (inclusive) of referenced data
+    end: DataIdx,  // Ending index (exclusive) of referenced data
+    node_id: NodeId, // ID of child node that the edge points to. This is a special value NODE_UNDEFINED for a leaf node.
 }
 
 impl SuffixTree {
-    pub fn new(data: Vec<u8>) -> Self {
-        SuffixTree {
-            tree: Tree::new(&data),
-            data,
-        }
-    }
-
-    pub fn lookup(&self, query: &[u8]) -> (usize, usize) {
-        self.tree.lookup(query, &self.data)
-    }
-}
-
-#[derive(PartialEq, Eq, Debug)]
-enum Tree {
-    Leaf(usize),  // address 
-    Node(HashMap<Option<u8>, Tree>),  // children by first byte
-}
-
-impl Tree {
     pub fn new(data: &[u8]) -> Self {
-        let mut tree = Tree::Leaf(0);
-        for i in 1..data.len() {
-            println!("{}", i);
-            Self::add_suffix(&mut tree, data, i);
+        let mut root_node = Node {
+            edges: Vec::new(),
+            edge_lookup: HashMap::new(),
+            suffix_link: NODE_UNDEFINED,
+        };
+        let mut tree = SuffixTree {
+            data: vec![],
+            nodes: vec![root_node],
+            cut: Position { node_id: 0, edge_id: EDGE_UNDEFINED, length: 0 },
+        };
+        for &b in data {
+            tree.push_byte(b);
         }
         tree
     }
 
-    fn add_suffix(mut tree: &mut Tree, data: &[u8], mut idx: usize) {
+    // Follow data[start..(start + length)] starting from given node, and return the resulting position.
+    fn follow_data(&self, mut node_id: NodeId, mut start: DataIdx, mut length: DataIdx) -> Position {
         loop {
-            assert!(idx <= data.len());
-            let b: Option<u8> = if idx == data.len() {
-                None
+            if length == 0 {
+                return Position {
+                    node_id,
+                    edge_id: EDGE_UNDEFINED,
+                    length: 0,
+                };
+            }
+            let node = &self.nodes[node_id as usize];
+            let b = self.data[start as usize];
+            let edge_id = node.edge_lookup[&b];
+            let edge = &node.edges[edge_id as usize];
+            let edge_len = edge.end - edge.start;
+            if length < edge_len || edge.end == DATA_END {
+                return Position {
+                    node_id,
+                    edge_id, 
+                    length,
+                };
+            }
+            start += edge_len;
+            length -= edge_len;
+            node_id = edge.node_id;
+        }
+    }
+
+    fn push_byte(&mut self, b: u8) {
+        // Append the byte to the data vector:
+        self.data.push(b);
+
+        // Now for every index i, we need to ensure that the substring data[i..] exists in the tree
+        // (where data[i..] now includes the newly added byte at the end).
+        // For i less than the cut position, this is already taken care of automatically, since in such cases
+        // data[i..] is represented by a leaf, which has an implicit end position (DATA_END).
+        loop {
+            let cut = &self.cut;
+            let node = &mut self.nodes[cut.node_id as usize];
+            if cut.length == 0 {
+                if let Some(&edge_id) = node.edge_lookup.get(&b) {
+                    // data[cut..] already existed in the tree. This means data[i..] also already existed for i > cut.
+                    // So we are done.
+                    return;
+                } else {
+                    // Add a new leaf edge containing the single new byte:
+                    let edge_id = node.edges.len() as EdgeId;
+                    let edge = Edge {
+                        start: (self.data.len() - 1) as DataIdx,
+                        end: DATA_END,
+                        node_id: NODE_UNDEFINED,
+                    };
+                    node.edges.push(edge);
+                    node.edge_lookup.insert(b, edge_id);
+
+                    // With the new byte, data[cut..] now becomes a leaf, so advance the cut position.
+                    if node.suffix_link == NODE_UNDEFINED {
+                        // The node doesn't have a suffix link; this should only happen at the root node.
+                        assert!(cut.node_id == 0);  // Check that this is in fact the root node.
+                        if cut.length == 0 {
+                            // data[cut..] is the empty string, so we are done.
+                            return;
+                        } else {
+                            // Locate the new cut position directly, by stripping off the first byte of data and
+                            // following a path down the tree, starting from the root.
+                            let edge = &node.edges[cut.edge_id as usize];
+                            self.cut = self.follow_data(0, edge.start + 1, cut.length - 1);
+                        }
+                    } else {
+                        // Follow the suffix link.
+                        self.cut = Position {
+                            node_id: node.suffix_link,
+                            edge_id: EDGE_UNDEFINED,
+                            length: 0,
+                        }
+                    }
+                }
             } else {
-                Some(data[idx])
-            };
-            match tree {
-                &mut Tree::Leaf(addr) => {
-                    if Some(data[addr]) == b {
-                        let mut new_map = HashMap::new();
-                        new_map.insert(b, Tree::Leaf(addr + 1));
-                        *tree = Tree::Node(new_map);
-                        tree = match tree {
-                            Tree::Node(m) => {
-                                m.get_mut(&b).unwrap()
-                            },
-                            _ => panic!("Unexpected error in add_prefix")
-                        };
-                        idx += 1;
-                        continue;
+                let edge = &mut node.edges[cut.edge_id as usize];
+                let c = self.data[(edge.start + cut.length) as usize];
+                if c == b {
+                    // data[cut..] already existed in the tree. This means data[i..] also already existed for i > cut.
+                    // So we are done.
+                    return;
+                } else {
+                    // The edge must be split and a new node created in the middle:
+                    let tail_edge = Edge {
+                        start: cut.length,
+                        end: edge.end,
+                        node_id: edge.node_id,
+                    };
+                    let leaf_edge = Edge {
+                        start: (self.data.len() - 1) as DataIdx,
+                        end: DATA_END,
+                        node_id: NODE_UNDEFINED
+                    };
+                    let mut new_node = Node {
+                        edges: vec![tail_edge, leaf_edge],
+                        edge_lookup: vec![(c, 0), (b, 1)].into_iter().collect(),
+                        suffix_link: NODE_UNDEFINED,
+                    };
+                    let new_node_id = self.nodes.len() as NodeId;
+                    edge.end = cut.length;
+                    edge.node_id = new_node_id;
+                    self.nodes.push(new_node);
+
+                    // With the new byte, data[cut..] now becomes a leaf, so advance the cut position
+                    // by following the suffix link, and populate the suffix link of the newly created node.
+                    let new_cut = self.follow_data(node.suffix_link, edge.start, cut.length);
+                    if new_cut.length == 0 {
+                        // A node for the new node's suffix link already exists.
+                        new_node.suffix_link = new_cut.node_id;
                     } else {
-                        let mut new_map: HashMap<Option<u8>, Tree> = HashMap::new();
-                        new_map.insert(Some(data[addr]), Tree::Leaf(addr + 1));
-                        new_map.insert(b, Tree::Leaf(idx + 1));
-                        *tree = Tree::Node(new_map);
-                        break;
+                        // A node for the new node's suffix link doesn't yet exist but will be created on the next loop iteration.
+                        // We already know what its ID will be.
+                        new_node.suffix_link = self.nodes.len() as NodeId;
                     }
-                },
-                Tree::Node(children) => {
-                    if children.contains_key(&b) {
-                        tree = children.get_mut(&b).unwrap();
-                        idx += 1;
-                        continue;
-                    } else {
-                        children.insert(b, Tree::Leaf(idx + 1));
-                        break;                            
-                    }
+                    self.cut = new_cut;
                 }
             }
         }
-    }
-
-    // Returns address and length of longest match.
-    fn lookup(&self, mut query: &[u8], data: &[u8]) -> (usize, usize) {
-        let mut tree = self;
-        let mut length = 0;
-        loop {
-            match tree {
-                &Tree::Leaf(addr) => {
-                    let max_len = std::cmp::min(query.len(), data.len() - addr);
-                    for i in 0..max_len {
-                        if query[i] != data[addr + i] {
-                            return (addr - length, length + i);
-                        }
-                    }
-                    return (addr - length, length + max_len);
-                },
-                Tree::Node(children) => {
-                    if query.len() > 0 && children.contains_key(&Some(query[0])) {
-                        tree = children.get(&Some(query[0])).unwrap();
-                        length += 1;
-                        query = &query[1..];
-                    } else {
-                        // We've found the longest possible match(es). Now just pick one.
-                        let mut skip_length = 1;
-                        tree = children.iter().next().unwrap().1;
-                        loop {
-                            match tree {
-                                &Tree::Leaf(addr) => {
-                                    return (addr - length - skip_length, length);
-                                },
-                                Tree::Node(children) => {
-                                    tree = children.iter().next().unwrap().1;
-                                    skip_length += 1;
-                                },
-                            }
-                        }
-
-                    }
-                }
-            }    
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_1() {
-        let data = [10, 20, 10];
-        let tree = Tree::new(&data);
-        assert_eq!(tree.lookup(&[20], &data), (1, 1));
-        assert_eq!(tree.lookup(&[10, 20], &data), (0, 2));
-        assert_eq!(tree.lookup(&[20, 10, 30], &data), (1, 2));
-        assert_eq!(tree.lookup(&[10, 20, 10], &data), (0, 3));
-    }
-
-    #[test]
-    fn test_2() {
-        let data = [10, 20, 30, 20, 50, 20, 50, 70];
-        let tree = Tree::new(&data);
-        assert_eq!(tree.lookup(&[30, 20, 50, 60], &data), (2, 3));
-        assert_eq!(tree.lookup(&[20, 50, 20, 80], &data), (3, 3));
     }
 }
