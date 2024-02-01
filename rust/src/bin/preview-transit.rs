13419e8a8d6d8ff59d24f37ff694170851b229d7
Brent Kerby <blkerby@gmail.com>
2024-01-31 20:24:26 -0700

    previewing transit tube rooms

--- rust/src/bin/preview-transit.rs
+++ rust/src/bin/preview-transit.rs
@@ -1,9 +1,30 @@
-use hashbrown::HashMap;
-use image::{Rgb, RgbImage};
-use maprando::game_data::smart_xml::{self, from_hex_words, BGData, Screen};
-use std::path::Path;
-
 use anyhow::{Context, Result};
+use clap::Parser;
+use hashbrown::{HashMap, HashSet};
+use image::codecs::png::PngReader;
+use maprando::game_data::{smart_xml, DoorPtr, RoomGeometry};
+use maprando::patch::{self, get_room_state_ptrs, pc2snes, snes2pc, Rom};
+use serde_derive::Deserialize;
+use smart_xml::{Layer2Type, Screen};
+use std::path::{Path, PathBuf};
+
+use image::io::Reader as ImageReader;
+use image::{GenericImageView, Rgb, RgbImage};
+use maprando::game_data::smart_xml::{from_hex_words, BGData};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    input_rom: PathBuf,
+}
+
+#[derive(Deserialize, Default, Clone, Debug)]
+struct TransitData {
+    name: String, // Intersecting room name (matching room geometry)
+    x: Vec<usize>,
+    top: String, // Transit tube theme above room (matching room name in TransitTube SMART project)
+    bottom: String, // Transit tube theme below room (matching room name in TransitTube SMART project)
+}
 
 #[derive(Copy, Clone)]
 struct Tile8x8 {
@@ -134,7 +155,7 @@ fn load_sce_tileset(tileset_path: &Path, cre_tileset: &CRETileset) -> Result<SCE
     let palette = load_palette(&tileset_path.join("palette.snes"))?;
     let mut gfx = load_8x8_gfx(&tileset_path.join("8x8tiles.gfx"))?;
     let sce_tiles = load_16x16_gfx(&tileset_path.join("16x16tiles.ttb"))?;
-    
+
     gfx.extend(cre_tileset.gfx.clone());
     let mut tiles = cre_tileset.tiles.clone();
     tiles.extend(sce_tiles);
@@ -145,7 +166,13 @@ fn load_sce_tileset(tileset_path: &Path, cre_tileset: &CRETileset) -> Result<SCE
     })
 }
 
-fn render_tile_8x8(image: &mut RgbImage, x0: usize, y0: usize, tile: Tile8x8, tileset: &SCETileset) {
+fn render_tile_8x8(
+    image: &mut RgbImage,
+    x0: usize,
+    y0: usize,
+    tile: Tile8x8,
+    tileset: &SCETileset,
+) {
     let gfx = &tileset.gfx[tile.idx];
     for y in 0..8 {
         for x in 0..8 {
@@ -162,7 +189,13 @@ fn render_tile_8x8(image: &mut RgbImage, x0: usize, y0: usize, tile: Tile8x8, ti
     }
 }
 
-fn render_tile_16x16(image: &mut RgbImage, x0: usize, y0: usize, tile: Tile16x16, tileset: &SCETileset) {
+fn render_tile_16x16(
+    image: &mut RgbImage,
+    x0: usize,
+    y0: usize,
+    tile: Tile16x16,
+    tileset: &SCETileset,
+) {
     render_tile_8x8(image, x0, y0, tile.top_left, tileset);
     render_tile_8x8(image, x0 + 8, y0, tile.top_right, tileset);
     render_tile_8x8(image, x0, y0 + 8, tile.bottom_left, tileset);
@@ -268,16 +301,64 @@ fn render_screens(screens: &[Screen], image: &mut RgbImage, tileset: &SCETileset
 
 fn render_tube(image: &mut RgbImage, screen_x: usize, tileset: &SCETileset) {
     let tube_left_tile = Tile16x16 {
-        top_left: Tile8x8 { idx: 0x377, palette: 0, flip_x: true, flip_y: false, priority: false },
-        top_right: Tile8x8 { idx: 0x376, palette: 0, flip_x: true, flip_y: false, priority: false },
-        bottom_left: Tile8x8 { idx: 0x377, palette: 0, flip_x: true, flip_y: false, priority: false },
-        bottom_right: Tile8x8 { idx: 0x376, palette: 0, flip_x: true, flip_y: false, priority: false },
+        top_left: Tile8x8 {
+            idx: 0x377,
+            palette: 0,
+            flip_x: true,
+            flip_y: false,
+            priority: false,
+        },
+        top_right: Tile8x8 {
+            idx: 0x376,
+            palette: 0,
+            flip_x: true,
+            flip_y: false,
+            priority: false,
+        },
+        bottom_left: Tile8x8 {
+            idx: 0x377,
+            palette: 0,
+            flip_x: true,
+            flip_y: false,
+            priority: false,
+        },
+        bottom_right: Tile8x8 {
+            idx: 0x376,
+            palette: 0,
+            flip_x: true,
+            flip_y: false,
+            priority: false,
+        },
     };
     let tube_right_tile = Tile16x16 {
-        top_left: Tile8x8 { idx: 0x376, palette: 0, flip_x: false, flip_y: false, priority: false },
-        top_right: Tile8x8 { idx: 0x377, palette: 0, flip_x: false, flip_y: false, priority: false },
-        bottom_left: Tile8x8 { idx: 0x376, palette: 0, flip_x: false, flip_y: false, priority: false },
-        bottom_right: Tile8x8 { idx: 0x377, palette: 0, flip_x: false, flip_y: false, priority: false },
+        top_left: Tile8x8 {
+            idx: 0x376,
+            palette: 0,
+            flip_x: false,
+            flip_y: false,
+            priority: false,
+        },
+        top_right: Tile8x8 {
+            idx: 0x377,
+            palette: 0,
+            flip_x: false,
+            flip_y: false,
+            priority: false,
+        },
+        bottom_left: Tile8x8 {
+            idx: 0x376,
+            palette: 0,
+            flip_x: false,
+            flip_y: false,
+            priority: false,
+        },
+        bottom_right: Tile8x8 {
+            idx: 0x377,
+            palette: 0,
+            flip_x: false,
+            flip_y: false,
+            priority: false,
+        },
     };
     for tile_y in 0..image.height() / 16 {
         let y = (tile_y * 16) as usize;
@@ -287,9 +368,15 @@ fn render_tube(image: &mut RgbImage, screen_x: usize, tileset: &SCETileset) {
     }
 }
 
-fn render_room(room_path: &Path, output_rooms_dir: &Path, tilesets: &HashMap<usize, SCETileset>, draw_tube: bool) -> Result<()> {
+fn render_room(
+    room_path: &Path,
+    output_rooms_dir: &Path,
+    tilesets: &HashMap<usize, SCETileset>,
+) -> Result<()> {
     let room_filename = room_path.file_name().unwrap().to_str().unwrap().to_owned();
-    let room_name = room_filename.strip_suffix(".xml").context("Expecting room filename to end in .xml")?;
+    let room_name = room_filename
+        .strip_suffix(".xml")
+        .context("Expecting room filename to end in .xml")?;
     let room_str = std::fs::read_to_string(&room_path)
         .with_context(|| format!("Unable to load room at {}", room_path.display()))?;
     let room: smart_xml::Room = serde_xml_rs::from_str(room_str.as_str())
@@ -298,6 +385,7 @@ fn render_room(room_path: &Path, output_rooms_dir: &Path, tilesets: &HashMap<usi
     if room.height < 2 {
         return Ok(());
     }
+    println!("Processing {}", room_path.display());
     for (state_idx, state_xml) in room.states.state.iter().enumerate() {
         if state_idx != 0 {
             // For now, skip room states other than the first one.
@@ -317,9 +405,7 @@ fn render_room(room_path: &Path, output_rooms_dir: &Path, tilesets: &HashMap<usi
 
             render_bgdata(&state_xml.bg_data, &mut image, tileset)?;
             render_screens(&state_xml.level_data.layer_2.screen, &mut image, tileset);
-            if draw_tube {
-                render_tube(&mut image, tube_screen_x, tileset);
-            }
+            render_tube(&mut image, tube_screen_x, tileset);
             render_screens(&state_xml.level_data.layer_1.screen, &mut image, tileset);
             image.save(output_rooms_dir.join(format!("{}-{}.png", room_name, tube_screen_x)))?;
         }
@@ -327,53 +413,180 @@ fn render_room(room_path: &Path, output_rooms_dir: &Path, tilesets: &HashMap<usi
     Ok(())
 }
 
+fn combine_images(
+    main_img: &RgbImage,
+    top_img: &RgbImage,
+    bottom_img: &RgbImage,
+    x_screen: usize,
+    y_min_screen: usize,
+    y_max_screen: usize,
+) -> Result<RgbImage> {
+    let mut img = RgbImage::new(256, 2560);
+
+    // Write main room image
+    let room_height_screens = (y_max_screen - y_min_screen + 1) as u32;
+    let room_offset_screens = 5 - room_height_screens / 2;
+    let room_height_pixels = room_height_screens * 256;
+    let room_offset_pixels = room_offset_screens * 256;
+    for y in 0..room_height_pixels {
+        for x in 0..256 {
+            let pixel = *main_img.get_pixel((x_screen as u32) * 256 + x, (y_min_screen as u32) * 256 + y);
+            img.put_pixel(x, y + room_offset_pixels, pixel);
+        }
+    }
+
+    // Write tube top image
+    for y_screen in 0..room_offset_screens {
+        let src_y_screen = if y_screen == room_offset_screens - 1 { 4 } else { y_screen };
+        for y in 0..256 {
+            for x in 0..256 {
+                let pixel = *top_img.get_pixel(x, src_y_screen * 256 + y);
+                img.put_pixel(x, y_screen * 256 + y, pixel);    
+            }
+        }
+    }
+
+    // Write tube bottom image
+    let num_screens_bottom = 10 - room_offset_screens - room_height_screens;
+    for y_screen in 0..num_screens_bottom {
+        let src_y_screen = if y_screen == num_screens_bottom - 1 { 9 } else { 5 + y_screen };
+        let dst_y_screen = y_screen + room_offset_screens + room_height_screens;
+        for y in 0..256 {
+            for x in 0..256 {
+                let pixel = *bottom_img.get_pixel(x, src_y_screen * 256 + y);
+                img.put_pixel(x, dst_y_screen * 256 + y, pixel);
+            }
+        }
+    }
+    
+    Ok(img)
+}
+
 fn main() -> Result<()> {
+    let args = Args::parse();
     let mosaic_dir = Path::new("../Mosaic");
     let projects_dir = mosaic_dir.join("Projects");
     let base_dir = projects_dir.join("Base");
-    let cre_tileset_dir = base_dir.join("Export/Tileset/CRE/00/");
-    // let sce_tilesets_dir = base_dir.join("Export/Tileset/SCE");
-    let sce_tilesets_dir = base_dir.join("Export/Tileset/SCE");
     let output_dir = Path::new("../tmp/preview");
-    let output_tilesets_dir = &output_dir.join("tilesets");
-    let output_rooms_dir = &output_dir.join("rooms");
+    let output_transit_dir = &output_dir.join("transit");
+    let room_preview_dir = &output_dir.join("rooms");
+    let room_geometry_path = Path::new("../room_geometry.json");
+    let transit_tube_data_path = Path::new("../transit-tube-data");
+    std::fs::create_dir_all(output_transit_dir)?;
 
-    std::fs::create_dir_all(output_tilesets_dir)?;
-    std::fs::create_dir_all(output_rooms_dir)?;
-    let cre_tileset = load_cre_tileset(&cre_tileset_dir)?;
-    let mut sce_tilesets: HashMap<usize, SCETileset> = HashMap::new();
-
-    for tileset_path in std::fs::read_dir(sce_tilesets_dir)? {
-        let tileset_path = tileset_path?;
-        let name = tileset_path.file_name().to_str().unwrap().to_owned();
-        let tileset_idx = usize::from_str_radix(&name, 16)?;
-        if tileset_idx >= 0x0F && tileset_idx <= 0x14 {
-            // Skip Ceres tilesets
-            continue;
-        }
-        let tileset = load_sce_tileset(&tileset_path.path(), &cre_tileset)?;
-
-        let img = render_tileset(&tileset);
-        img.save(output_tilesets_dir.join(format!("{}.png", tileset_idx)))?;
-
-        sce_tilesets.insert(tileset_idx, tileset);
+    // Index SMART project rooms:
+    let mut room_name_by_pair: HashMap<(usize, usize), String> = HashMap::new();
+    for room_path in std::fs::read_dir(base_dir.join("Export/Rooms"))? {
+        let room_path = room_path?.path();
+        let room_filename = room_path.file_name().unwrap().to_str().unwrap().to_owned();
+        let room_name = room_filename
+            .strip_suffix(".xml")
+            .context("Expecting room filename to end in .xml")?;
+        // println!("Room: {}", room_name);
+        let room_str = std::fs::read_to_string(&room_path)
+            .with_context(|| format!("Unable to load room at {}", room_path.display()))?;
+        let room: smart_xml::Room = serde_xml_rs::from_str(room_str.as_str())
+            .with_context(|| format!("Unable to parse XML in {}", room_path.display()))?;
+        room_name_by_pair.insert((room.area, room.index), room_name.to_string());
     }
 
-    for theme_dir in std::fs::read_dir(projects_dir)? {
-        let theme_dir = theme_dir?;
-        let theme_path = theme_dir.path();
-        let theme_name = theme_dir.file_name().to_str().unwrap().to_owned();
-        println!("Theme: {}", theme_name);
-        let theme_rooms_path = theme_path.join("Export/Rooms");
-        let output_theme_path = output_rooms_dir.join(&theme_name);
-        std::fs::create_dir_all(&output_theme_path)?;
-        for room_path in std::fs::read_dir(theme_rooms_path)? {
-            let room_path = room_path?.path();
-            println!("Room: {}", room_path.to_str().unwrap());
-            render_room(&room_path, &output_theme_path, &sce_tilesets, theme_name != "TransitTube")
-              .context(format!("Error rendering room: {}", room_path.display()))?;
-        }
+    let room_geometry_str = std::fs::read_to_string(room_geometry_path).with_context(|| {
+        format!(
+            "Unable to load room geometry at {}",
+            room_geometry_path.display()
+        )
+    })?;
+    let room_geometry: Vec<RoomGeometry> = serde_json::from_str(&room_geometry_str)?;
+    let mut room_idx_by_name: HashMap<String, usize> = HashMap::new();
+    for (i, room) in room_geometry.iter().enumerate() {
+        room_idx_by_name.insert(room.name.clone(), i);
     }
 
+    let rom = Rom::load(&args.input_rom)?;
+
+    let theme_name = "Base";
+    let theme_transit_data_path = transit_tube_data_path.join(format!("{}.json", theme_name));
+    let theme_transit_data_str =
+        std::fs::read_to_string(&theme_transit_data_path).with_context(|| {
+            format!(
+                "Unable to load transit tube data at {}",
+                theme_transit_data_path.display()
+            )
+        })?;
+    let theme_transit_data_vec: Vec<TransitData> = serde_json::from_str(&theme_transit_data_str)?;
+    let theme_transit_dir = &output_transit_dir.join(theme_name);
+    std::fs::create_dir_all(theme_transit_dir)?;
+
+    for transit_data in &theme_transit_data_vec {
+        println!("{}", transit_data.name);
+        let room_idx = room_idx_by_name[&transit_data.name];
+        let room_geometry = &room_geometry[room_idx];
+        let room_ptr = room_geometry.rom_address;
+        let room_area = rom.read_u8(room_ptr + 1)? as usize;
+        let room_index = rom.read_u8(room_ptr)? as usize;
+        let smart_room_name = &room_name_by_pair[&(room_area, room_index)];
+
+        for &x in &transit_data.x {
+            let mut y_min = usize::MAX;
+            let mut y_max = 0;
+            for y in 0..room_geometry.map.len() {
+                if room_geometry.map[y][x] == 1 {
+                    if y < y_min {
+                        y_min = y;
+                    }
+                    if y > y_max {
+                        y_max = y;
+                    }
+                }
+            }
+
+            let main_room_png_path = room_preview_dir
+                .join(theme_name)
+                .join(format!("{}-{}.png", smart_room_name, x));
+            println!(
+                "main (x={}, y_min={}, y_max={}): {}",
+                x,
+                y_min,
+                y_max,
+                main_room_png_path.display()
+            );
+            let main_room_img = ImageReader::open(&main_room_png_path)?.decode()?.to_rgb8();
+
+            let tube_theme_top = transit_data.top.to_ascii_uppercase();
+            let tube_top_png_path = room_preview_dir
+                .join("TransitTube")
+                .join(format!("{}-0.png", tube_theme_top));
+            println!("top: {}", tube_top_png_path.display());
+            let tube_top_img = ImageReader::open(&tube_top_png_path)?.decode()?.to_rgb8();
+
+            let tube_theme_bottom = transit_data.bottom.to_ascii_uppercase();
+            let tube_bottom_png_path = room_preview_dir
+                .join("TransitTube")
+                .join(format!("{}-0.png", tube_theme_bottom));
+            println!("bottom: {}", tube_bottom_png_path.display());
+            let tube_bottom_img = ImageReader::open(&tube_bottom_png_path)?
+                .decode()?
+                .to_rgb8();
+
+            let combined_img = combine_images(
+                &main_room_img,
+                &tube_top_img,
+                &tube_bottom_img,
+                x,
+                y_min,
+                y_max,
+            )?;
+            let combined_path = output_transit_dir
+                .join(theme_name)
+                .join(format!("{}-{}.png", smart_room_name, x));
+            combined_img.save(combined_path)?;
+        }
+    }
+    // println!("{:?}", transit_data);
+
+    // for room_path in std::fs::read_dir(base_rooms_dir)? {
+    //     let room_path = room_path?.path();
+    //     render_room(&room_path, &output_rooms_dir, &sce_tilesets)?;
+    // }
     Ok(())
 }
