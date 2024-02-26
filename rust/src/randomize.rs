6f3ef9925269b3d196637c7d7f46f11cb20e9a0c
Brent Kerby <blkerby@gmail.com>
2024-02-26 12:12:20 -0700

    remove one-way-reachable items when using early stop item placement

--- rust/src/randomize.rs
+++ rust/src/randomize.rs
@@ -3072,7 +3072,7 @@ impl<'r> Randomizer<'r> {
                 remaining_items
             );    
             for item_loc_state in &mut state.item_location_state {
-                if item_loc_state.placed_item.is_none() {
+                if item_loc_state.placed_item.is_none() || !item_loc_state.bireachable {
                     item_loc_state.placed_item = Some(Item::Nothing);
                 }
             }
