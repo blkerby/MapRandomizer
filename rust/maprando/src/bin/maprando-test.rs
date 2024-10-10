630aededfe3dad55f1a419af076024756c0e8546
Brent Kerby <blkerby@gmail.com>
2024-10-10 12:06:27 -0600

    put Bombs early in Normal item progression

--- rust/maprando/src/bin/maprando-test.rs
+++ rust/maprando/src/bin/maprando-test.rs
@@ -210,7 +210,7 @@ fn set_item_progression_normal(diff: &mut DifficultyConfig) -> () {
     diff.item_priorities = vec![
         ItemPriorityGroup {
             name: "Early".to_string(),
-            items: vec!["Morph"].into_iter().map(|x| x.to_string()).collect(),
+            items: vec!["Morph", "Bombs"].into_iter().map(|x| x.to_string()).collect(),
         },
         ItemPriorityGroup {
             name: "Default".to_string(),
@@ -224,7 +224,6 @@ fn set_item_progression_normal(diff: &mut DifficultyConfig) -> () {
                 "Wave",
                 "Spazer",
                 "Plasma",
-                "Bombs",
                 "Grapple",
                 "HiJump",
                 "SpeedBooster",
