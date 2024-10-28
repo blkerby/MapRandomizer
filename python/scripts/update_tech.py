f5a978f3f0750a26656edb9a725ea856405c2328
Brent Kerby <blkerby@gmail.com>
2024-10-28 15:24:20 -0600

    update skill presets in update_tech

--- python/scripts/update_tech.py
+++ python/scripts/update_tech.py
@@ -4,7 +4,27 @@ import json
 
 sm_json_path = pathlib.Path("sm-json-data")
 output_path = pathlib.Path("rust/data/tech_data.json")
+skill_presets_path = pathlib.Path("rust/data/presets/skill-assumptions")
 videos_url = "https://videos.maprando.com"
+
+tech_json_path = sm_json_path / "tech.json"
+tech_json = json.load(open(tech_json_path, "r"))
+
+# Extract all tech IDs in the order listed in sm-json-data:
+tech_id_order = []
+def get_tech_ids(t):
+    global tech_id_order
+    if "id" in t:
+        tech_id_order.append(t["id"])
+    else:
+        print("Tech {} has no 'id'".format(t["name"]))
+    for e in t.get("extensionTechs", []):
+        get_tech_ids(e)
+        
+for c in tech_json["techCategories"]:
+    for t in c["techs"]:
+        get_tech_ids(t)
+
 
 tech_data = requests.get(videos_url + "/tech").json()
 
@@ -16,7 +36,47 @@ tech_data.append({
     "video_id": None,
 })
 
-json.dump(tech_data, open(output_path, "w"), indent=2)
+# json.dump(tech_data, open(output_path, "w"), indent=2)
 
+difficulty_levels = [
+    "Implicit",
+    "Basic",
+    "Medium",
+    "Hard",
+    "Very Hard",
+    "Expert",
+    "Extreme",
+    "Insane",
+    "Beyond",
+    "Ignored",
+]
 
-# TODO: update skill presets and full presets
+tech_dict = {t["tech_id"]: t for t in tech_data}
+tech_id_by_difficulty = {d: [] for d in difficulty_levels}
+
+for tech_id in tech_id_order:
+    tech = tech_dict[tech_id]
+    difficulty = tech["difficulty"]
+    if difficulty not in tech_id_by_difficulty:
+        print("Unrecognized difficulty {} for tech {}".format(difficulty, tech["name"]))
+    tech_id_by_difficulty[difficulty].append(tech_id)
+
+for preset_difficulty_idx in range(0, 9):  # skip Ignored difficulty
+    preset_difficulty = difficulty_levels[preset_difficulty_idx]
+    path = skill_presets_path / f'{preset_difficulty}.json'
+    preset = json.load(open(path, 'r'))
+    tech_settings = []
+    for tech_difficulty_idx in range(1, 9):  # skip Implicit and Ignored difficulties
+        tech_difficulty = difficulty_levels[tech_difficulty_idx]
+        for tech_id in tech_id_by_difficulty[tech_difficulty]:
+            tech = tech_dict[tech_id]
+            tech_settings.append({
+                "id": tech_id,
+                "name": tech_data["name"],
+                "enabled": tech_difficulty_idx <= preset_difficulty_idx
+            })
+    preset["tech_settings"] = tech_settings
+
+    json.dump(preset, open(path, "w"))
+
+# TODO: full presets
