1ce42599d3e6d2066fe3d69fbce45380a312630c
jeboo <jeb00o@yahoo.com>
2024-11-26 15:24:34 -0500

    restore MB escape door when entering from left (post-MB1)

--- patches/src/mb_left_entrance.asm
+++ patches/src/mb_left_entrance.asm
@@ -1,6 +1,9 @@
 arch snes.cpu
 lorom
 
+!bank_84_free_space_start = $84f580
+!bank_84_free_space_end = $84f5a0
+
 org $83AAEA
     dw $EE00               ; Set door ASM for Tourian Escape Room 1 toward Mother Brain
 
@@ -8,3 +11,41 @@ org $8FEE00
     JSL $8483D7            ;\
     db  $00, $06           ;|
     dw  $B677              ;} Spawn Mother Brain's room escape door
+
+; Restore escape door after MB1 in case of left entry
+org $ADE3D1                ; hook MB1 => MB2 transition
+    jsl fix_mb_escape_door
+
+; instruction set to restore door
+org $8494C9
+    dw $8004, $8b0f, $8ae8, $82e8, $830f
+    dw $0001
+    dw $8004, $8b0f, $8ae8, $82e8, $830f
+    dw $0000
+
+org $84AC01
+    dw #$94c9              ; switch to unused instruction set with adequate space
+
+org $84B66F                ; custom PLM in unused space
+    dw #$b3c1, #$abff      ; deactivate PLMs, reference above instruction set
+
+org !bank_84_free_space_start
+fix_mb_escape_door:
+    pha
+    lda $af6
+    cmp #$0025             ; check if samus is beyond left wall edge
+    bcs .no_fix
+    lda #$0025             ; push samus out of door before redrawing
+    sta $af6
+    stz $af8
+
+.no_fix
+    jsl $8483d7            ; custom PLM that restores escape door
+    db  $00, $06
+    dw  $b66f
+
+    pla
+    sta $fa8               ; replaced code
+    rtl
+
+warnpc !bank_84_free_space_end
