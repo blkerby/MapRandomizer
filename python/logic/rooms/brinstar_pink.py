9dfee226f2f1724658f7ea1041e96a4219f62acd
Brent Kerby <blkerby@gmail.com>
2024-12-07 17:59:04 -0700

    update node tiles

--- python/logic/rooms/brinstar_pink.py
+++ python/logic/rooms/brinstar_pink.py
@@ -98,7 +98,6 @@ rooms = [
             12: [(2, 7)],  # charge beam
             13: [(2, 0), (3, 0), (2, 1), (3, 1), (2, 2), (3, 2),  # main junction
                  (2, 3), (3, 3), (2, 4), (3, 4), (2, 5), (3, 5), (2, 6)],
-            14: [(2, 4)],  # junction above crumbles
             15: [(3, 6)],  # G-Mode Morph Junction (Middle Right)
         },
     ),
