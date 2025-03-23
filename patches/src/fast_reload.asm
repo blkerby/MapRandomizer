883900d279f1154c2742fba6147309bee8ed541a
Brent Kerby <blkerby@gmail.com>
2025-03-23 13:31:56 -0600

    move stz bash5f5 to earlier in quick reload, hopefully avoids audio-related crashes

--- patches/src/fast_reload.asm
+++ patches/src/fast_reload.asm
@@ -142,6 +142,9 @@ hook_main:
     plp
     stz $0727    ; Reset pause menu index
     stz $0797    ; Reset door transition flag
+    lda #$0000
+    sta $7EC400  ; clear palette change numerator, in case of reload during fade-in/fade-out
+    stz $05F5    ; enable sounds
     pea $f70e    ; $82f70e = rtl
     jml !deathhook82
 
@@ -184,8 +187,6 @@ setup_music:
 
 setup_game_1:
     jsl $82be17       ; Stop sounds
-    lda #$0000
-    sta $7EC400       ; clear palette change numerator, in case of reload during fade-in/fade-out
     lda #$ffff
     sta !loadback_ready   ; Set the state that allows loading back to previous save.
     lda !QUICK_RELOAD
@@ -194,7 +195,6 @@ setup_game_1:
     sta $05f5
     jml $80a09b
 .quick
-    stz $05F5       ; Enable sounds (in case they are disabled due to reloading during door transition)
     jsl $80835d
     jsl $80985f
     jsl $82e76b
