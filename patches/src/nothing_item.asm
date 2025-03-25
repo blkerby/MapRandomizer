59b7f2b369b22b3696d16e09132c47cc62a4ae48
Brent Kerby <blkerby@gmail.com>
2025-03-24 20:31:02 -0600

    fix nothing item graphics (for BT room)

--- patches/src/nothing_item.asm
+++ patches/src/nothing_item.asm
@@ -9,7 +9,7 @@ org !bank_84_free_space_start
 dw $EE64, inst        ; PLM $F700 (nothing, for Bomb Torizo Room)
 
 inst:
-    dw $8764, $9200, $0000, $0000, $0000, $0000  ; Load item graphics (zeros)
+    dw $8764, $9700, $0000, $0000, $0000, $0000  ; Load item graphics (zeros)
     dw $8A24, .triggered                   ; Set link instruction for when triggered
     dw $86C1, $DF89                        ; Pre-instruction = go to link instruction if triggered
     dw $E04F                               ; Draw item frame 0 (not sure why we need to do this, but it doesn't work otherwise)
