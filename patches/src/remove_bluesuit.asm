3443e8e8cfbcf065fcb9ffcf1493428dacb38222
nn <53490794+nn357@users.noreply.github.com>
2026-01-15 18:43:12 +0900

    update bluesuit patch

--- patches/src/remove_bluesuit.asm
+++ patches/src/remove_bluesuit.asm
@@ -3,8 +3,8 @@
 arch snes.cpu
 lorom
 
-org $91D35C ; replace branch check of samus running momentum flag
+org $91DE59 ; replace branch check of samus running momentum flag
     STZ $0B3E
     LDA $0B3C
-    BEQ $DE8D
+    BEQ $2C
     STZ $0B3C
