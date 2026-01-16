fd81a13252d8c2ce7b1a0959e93c3726bda989f9
nn <53490794+nn357@users.noreply.github.com>
2026-01-16 17:53:30 +0900

    Update remove_bluesuit.asm

--- patches/src/remove_bluesuit.asm
+++ patches/src/remove_bluesuit.asm
@@ -3,8 +3,11 @@
 arch snes.cpu
 lorom
 
-org $91DE59 ; replace branch check of samus running momentum flag
+org $91DE59         ; replace branch check of samus running momentum flag
     STZ $0B3E
     LDA $0B3C
-    BEQ $2C
+    BEQ merge       ; replace coded branch with a label for readability / incase things ever get moved around.
     STZ $0B3C
+
+org $91DE8D
+merge:
