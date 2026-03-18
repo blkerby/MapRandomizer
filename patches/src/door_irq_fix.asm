feb6ec54d143257934c51f1430c6564a3d406843
nn <53490794+nn357@users.noreply.github.com>
2026-03-19 00:01:06 +0900

    remove hooks to external code.
    routine is now self contained by removing duplicate code.
    
    Co-Authored-By: jeboo <jeboo@users.noreply.github.com>

--- patches/src/door_irq_fix.asm
+++ patches/src/door_irq_fix.asm
@@ -4,22 +4,81 @@
 
 lorom
 
-org $8483fc ; ldy #$0002
-jmp disable_irq
-
-org $848419 ; $84:8419 ; sta $1c87,x
-jmp enable_irq
-
-
-org $84F4A0
-disable_irq:
-  sei
+org $8483d7
+  phb
+  phy
+  phx
+  phk
+  plb
+  ldy #$004e
+loop:
+  lda $1c37,y
+  beq found
+  dey
+  dey
+  bpl loop
+  lda $06,s
+  clc
+  adc #$0004
+  sta $06,s
+  sec
+  bra finished
+found:
+  sep #$20
+  lda $08,s
+  pha
+  plb
+  tyx
   ldy #$0002
-  jmp $83FF
-
-enable_irq:
+  sei
+  lda ($06,s),y
+  sta $4202
+  lda $07a5
+  sta $4203
+  ldy #$0001
+  lda ($06,s),y
+  rep #$20
+  and #$00FF
+  clc
+  adc $4216
+  asl a
   cli
   sta $1c87,x
-  jmp $841C
+  ldy #$0003
+  lda ($06,s),y
+  txy
+  tax
+  lda $06,s
+  clc
+  adc #$0004
+  sta $06,s
+  phk
+  plb
+  txa
+  sta $1c37,y
+  tyx
+  tay
+  lda #$0000
+  sta $1dc7,x
+  sta $7edf0c,x
+  lda #$8469
+  sta $1cd7,x
+  lda $0002,y
+  sta $1d27,x
+  lda #$0001
+  sta $7ede1c,x
+  lda #$8da0
+  sta $7ede6c,x
+  stz $1d77,x
+  stx $1c27
+  tyx
+  ldy $1c27
+  jsr ($0000,x)
+  clc
+finished:
+  plx
+  ply
+  plb
+  rtl
 
-assert pc() <= $84F500 ; 84F4AA
+assert pc() <= $848469
