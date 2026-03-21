c51abf8fbbc75d62061af8daa24e749c7aaecfc1
nn <53490794+nn357@users.noreply.github.com>
2026-03-21 21:41:47 +0900

    fix xmode/momentum conservation with sparkbooster (#321)

--- patches/src/momentum_conservation.asm
+++ patches/src/momentum_conservation.asm
@@ -1,17 +1,8 @@
 ; Patch to conserve Samus' horizontal momentum when landing while running
 ; Author: Scyzer, with updates by Nodever2 & OmegaDragnet7
 lorom
-
 !bank_90_free_space_start = $90F800
 !bank_90_free_space_end = $90F880
-
-!equipped_items = $09A2
-!blue_booster = $0040
-!spark_booster = $0080
-!speed_booster = $2000
-!any_booster = !blue_booster|!spark_booster|!speed_booster
-
-lorom
 
 ; ASM to keep running speed while landing. This is more effective than simply changing the pose table,
 ; as you can have variables, and doesn't move you forward a pixel.
@@ -65,15 +56,6 @@ SPEEDKEEP:
 	LDA $0B40 : BEQ + : LDA #$0003 : JSL $80914D ; resume speed booster sfx if needed
 +	; next two lines of code are mostly only needed because of a dumb vanilla bug with $91DA74
 
-    lda !equipped_items
-    bit #!blue_booster|!speed_booster
-    bne .skip
-    sep #$20
-    lda #$01
-    sta $0b3e
-    rep #$20
-    
-.skip:
 	LDA #$0001 : STA $0AD0 ; update samus palette next frame
 	LDA #$0004 : STA $0ACE ; reset samus speed booster/screw attack palette index
 	PLP : RTS
