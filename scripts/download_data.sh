d0c4a65f0effcdfe3b3a2bb6c938fd598f05ec66
Brent Kerby <blkerby@gmail.com>
2026-09-05 13:10:25 -0600

    In CI/non-interactive environments, suppress download progress updates

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# `wget` writes progress to stderr; suppress it outside a terminal (e.g. in CI).
+wget_args=()
+if [[ ! -t 2 ]]; then
+    wget_args+=(--no-verbose)
+fi
+
 mkdir -p tmp
 cd tmp
 
@@ -8,14 +14,14 @@ cd tmp
 mkdir -p ../maps
 for pool in "v119-small-avro" "v119-standard-avro" "v119-wild-avro"
 do
-wget https://map-rando-artifacts.s3.us-west-004.backblazeb2.com/maps/${pool}.tar
+wget "${wget_args[@]}" https://map-rando-artifacts.s3.us-west-004.backblazeb2.com/maps/${pool}.tar
 tar xf ${pool}.tar --directory ../maps
 rm ${pool}.tar
 done
 
 # Download the Mosaic patches
 export MOSAIC_BUILD_ID=$(cat ../MOSAIC_BUILD_ID)
-wget https://map-rando-artifacts.s3.us-west-004.backblazeb2.com/Mosaic/Mosaic-${MOSAIC_BUILD_ID}.tar.zstd
+wget "${wget_args[@]}" https://map-rando-artifacts.s3.us-west-004.backblazeb2.com/Mosaic/Mosaic-${MOSAIC_BUILD_ID}.tar.zstd
 zstd -d Mosaic-${MOSAIC_BUILD_ID}.tar.zstd -o Mosaic-${MOSAIC_BUILD_ID}.tar
 mkdir -p ../patches/mosaic
 tar xf Mosaic-${MOSAIC_BUILD_ID}.tar --directory ../patches/mosaic
